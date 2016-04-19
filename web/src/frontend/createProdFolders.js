10fceaa4a520a87f1dc8471a55d7a600adad5219
Helge Fredheim <helge.fredheim@nav.no>
2016-04-19 15:33:40 +0200

    Byggescript lager nå nødvendige mapper selv

---
+++ web/src/frontend/createProdFolders.js
@@ -0,0 +1,13 @@
+var fs = require("fs");
+
+var createFolderIfItDoesntExist = function(folder) {
+	fs.exists("../main/webapp/" + folder, function(exists) {
+		if(!exists) {
+			fs.mkdir("../main/webapp/" + folder)
+		}
+	})
+}
+
+createFolderIfItDoesntExist("styles");
+createFolderIfItDoesntExist("js");
+createFolderIfItDoesntExist("vendor");

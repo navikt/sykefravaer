1f942342740888df10b0afc26f0b1e46e5672dd0
Helge Fredheim <helge.fredheim@nav.no>
2016-04-19 10:08:31 +0200

    Implementering av brødsmuler. Brødsmulene er pt. hardkodet.

--- web/src/frontend/tests/components/BrodsmulerTest.js
+++ web/src/frontend/tests/components/BrodsmulerTest.js
@@ -1,54 +1,34 @@
 var expect = require("chai").expect;
 import Brodsmuler, {getSti} from "../../js/components/Brodsmuler.js";
 
-xdescribe("getSti", () => {
+xdescribe("getSti", () => { 
 
     it("Should return the proper path for Dine sykmeldinger", function () {
-        expect(getSti("/dine-sykmeldinger", {sykmeldingId: "3"})).to.deep.equal([{
+        expect(getSti("/app", {sykmeldingId: "3"})).to.deep.equal([{
             tittel: "Ditt NAV",
             erKlikkbar: true,
-            sti: "/"
+            sti: "/dittnav"
         }, {
             tittel: "Dine sykmeldinger",
-            sti: "/dine-sykmeldinger",
+            sti: "/app",
             erKlikkbar: false
         }])
     });
 
     it("Should return the proper path for Din sykmelding", function () {
-        expect(getSti("/dine-sykmeldinger/:sykmeldingId", {sykmeldingId: "3"})).to.deep.equal([{
+        expect(getSti("/app/sykmeldinger/:sykmeldingId", {sykmeldingId: "3"})).to.deep.equal([{
             tittel: "Ditt NAV",
             erKlikkbar: true,
-            sti: "/"
+            sti: "/dittnav"
         }, {
             tittel: "Dine sykmeldinger",
-            sti: "/dine-sykmeldinger",
+            sti: "/app/sykmeldinger",
             erKlikkbar: true
         }, {
             tittel: "Sykmelding",
-            sti: "/dine-sykmeldinger/3",
+            sti: "/app/sykmeldinger/3",
             erKlikkbar: false
         }])
     });
 
-    it("Should return the proper path for Send til arbeidsgiver", function () {
-        expect(getSti("/dine-sykmeldinger/:sykmeldingId/send-til-arbeidsgiver", {sykmeldingId: "3"})).to.deep.equal([{
-            tittel: "Ditt NAV",
-            erKlikkbar: true,
-            sti: "/"
-        }, {
-            tittel: "Dine sykmeldinger",
-            sti: "/dine-sykmeldinger",
-            erKlikkbar: true
-        }, {
-            tittel: "Sykmelding",
-            sti: "/dine-sykmeldinger/3",
-            erKlikkbar: true
-        }, {
-            tittel: "Send til arbeidsgiver",
-            sti: "/dine-sykmeldinger/3/send-til-arbeidsgiver",
-            erKlikkbar: false
-        }])
-    });
-
-});
+}); 

a47869db8de9d0c36c8d0a120ee7072626fba6c1
Helge Fredheim <helge.fredheim@nav.no>
2016-04-20 11:40:01 +0200

    La inn logging i bundle for enklere feilsøking

--- web/src/frontend/deploy.js
+++ web/src/frontend/deploy.js
@@ -2,30 +2,44 @@
 // locally we make sure it only runs in production
 if (process.env.NODE_ENV === 'production') {
 
+	console.log("env === 'production'");
+
 	// We basically just create a child process that will run
 	// the production bundle command
 	var child_process = require('child_process');
 
 	var fs = require("fs");
 
+	console.log("Sjekker om mappe finnes")
+
+	var bundle = function() {
+		child_process.exec("webpack -p --config webpack.production.config.js", function (error, stdout, stderr) {
+			console.log('stdout: ' + stdout);
+			console.log('stderr: ' + stderr);
+			if (error !== null) {
+				console.log('exec error: ' + error);
+			}
+		});
+	}
+
 	fs.exists("../main/webapp/js", function(exists) {
 
 		if(!exists) {
+			console.log("JS-mappe finnes ikke, og vi lager den")
 			// Lager JS-mappe hvis den ikke finnes
 			fs.mkdir("../main/webapp/js", function() {
-
+				console.log("JS-mappe laget")
 				// Når det er gjort, utfører vi bundling
-
-				child_process.exec("webpack -p --config webpack.production.config.js", function (error, stdout, stderr) {
-					console.log('stdout: ' + stdout);
-					console.log('stderr: ' + stderr);
-					if (error !== null) {
-						console.log('exec error: ' + error);
-					}
-				});
+				bundle(); 
 			})
+		} else {
+			console.log("JS-mappe finnes, og vi bundler direkte")
+			bundle(); 
 		}
 	})
 
+} else {
+
+	console.log("env !== 'production'");
 
 }

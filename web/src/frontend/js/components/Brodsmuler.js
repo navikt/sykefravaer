65ccca2e0e0d5dc4c864c6d6052a7d713e5388db
Helge Fredheim <helge.fredheim@nav.no>
2016-04-21 17:06:56 +0200

    Rettet bug som gjorde at lenker i brodsmulemeny ble lysebla pa mobil

--- web/src/frontend/js/components/Brodsmuler.js
+++ web/src/frontend/js/components/Brodsmuler.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router';
 import { getContextRoot } from '../routers/paths.js';
 
 const Brodsmuler = ({ brodsmuler }) => {
-    return (<nav role="navigation" className="brodsmuler blokk" aria-label="Du er her: ">
+    return (<nav role="navigation" className="brodsmuler blokk side-innhold" aria-label="Du er her: ">
         <img src="/sykefravaer/img/svg/account-circle.svg" alt="Du" className="brodsmuler-ikon" />
-        <a href="/dittnav" className="lenke-fremhevet">Ditt NAV</a><span className="brodsmule-skille"> / </span>
+        <a href="/dittnav">Ditt NAV</a><span className="brodsmule-skille"> / </span>
         {brodsmuler.map((smule, idx) => {
             return (smule.erKlikkbar ? <span key={idx}>
-                <Link to={getContextRoot() + smule.sti} className="lenke-fremhevet">{smule.tittel}</Link>
+                <Link to={getContextRoot() + smule.sti}>{smule.tittel}</Link>
                 <span className="brodsmule-skille"> / </span>
                 </span> : <span key={idx}>{smule.tittel}</span>);
         })}

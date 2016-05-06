d1ff43a39c29fdd985f9ae195f3385293e4bd10f
Helge Fredheim <helge.fredheim@nav.no>
2016-05-06 15:23:14 +0200

    Rettet feil som gjorde at checkbox-ikon rendret dårlig i IE

--- web/src/frontend/js/components/SykmeldingCheckbox.js
+++ web/src/frontend/js/components/SykmeldingCheckbox.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 export const SykmeldingCheckbox = ({ tekst, jsClassName, className }) => {
-    return <p className={'sykmelding-checkbox' + ( className ? ' ' + className : '' ) + ' js-' + jsClassName}>
+    return <p className={'sykmelding-checkbox' + ( className ? ' ' + className : '' ) + ' js-' + jsClassName}>	
                 <img src="/sykefravaer/img/svg/check-box-1.svg" className="ikon" alt="Avkrysset" />
                 <img src="/sykefravaer/img/svg/check-box-1-highcontrast.svg" className="ikon-hoykontrast" alt="Avkrysset" />
-                {tekst}
+                <span className="label">
+                	{tekst}
+                </span>
             </p>
 }
 

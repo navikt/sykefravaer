19552454f94d6a3498851a652af3cbde91557319
Helge Fredheim <helge.fredheim@nav.no>
2016-05-09 14:55:04 +0200

    Rettet feil sti på PNG-hake

--- web/src/frontend/js/components/SykmeldingCheckbox.js
+++ web/src/frontend/js/components/SykmeldingCheckbox.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export const SykmeldingCheckbox = ({ tekst, jsClassName, className }) => {
     return <p className={'sykmelding-checkbox' + ( className ? ' ' + className : '' ) + ' js-' + jsClassName}>	
-                <img src="/sykefravaer/img/svg/check-box-1.png" className="ikon" alt="Huket av" />
-                <img src="/sykefravaer/img/svg/check-box-1-highcontrast.png" className="ikon-hoykontrast" alt="Huket av" />
+                <img src="/sykefravaer/img/png/check-box-1.png" className="ikon" alt="Huket av" />
+                <img src="/sykefravaer/img/png/check-box-1-highcontrast.png" className="ikon-hoykontrast" alt="Huket av" />
                 <span className="label">
                 	{tekst}
                 </span>

05ea8381f4bec47c0117982db0a9919c1d61a6fc
Helge Fredheim <helge.fredheim@nav.no>
2016-05-10 12:37:31 +0200

    Rettet feilende test, og la til test for skuddår

--- web/src/frontend/tests/utils/utilsTest.js
+++ web/src/frontend/tests/utils/utilsTest.js
@@ -3,12 +3,16 @@ import { formatDate, getDuration } from '../../js/utils';
 
 describe("formatDate", () => {
 	it("Skal formatere dato", () => {
-		expect(formatDate("2014-02-27T23:00:00.000Z")).to.equal("26.02.2014");
+		expect(formatDate("2014-02-27T23:00:00.000Z")).to.equal("28.02.2014");
 	});
 
 	it("Skal ta hensyn til tidssoner", () => {
 		expect(formatDate("2016-05-10T10:23:48.355Z")).to.equal("10.05.2016");
 	}); 
+
+	it("Skal ta hensyn til skuddår", () => {
+		expect(formatDate("1984-02-28T23:00:00.000Z")).to.equal("29.02.1984");
+	});
 });
 
 describe("getDuration", () => {

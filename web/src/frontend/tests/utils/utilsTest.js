01d6260a082eb8f91574092fb2ec3679f58ee234
Helge Fredheim <helge.fredheim@nav.no>
2016-05-10 12:33:01 +0200

    Fjernet .only i test

--- web/src/frontend/tests/utils/utilsTest.js
+++ web/src/frontend/tests/utils/utilsTest.js
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import { formatDate, getDuration } from '../../js/utils';
 
-describe.only("formatDate", () => {
+describe("formatDate", () => {
 	it("Skal formatere dato", () => {
 		expect(formatDate("2014-02-27T23:00:00.000Z")).to.equal("26.02.2014");
 	});
@@ -11,7 +11,7 @@ describe.only("formatDate", () => {
 	}); 
 });
 
-describe.only("getDuration", () => {
+describe("getDuration", () => {
 	it("Skal regne ut varighet", () => {
 		expect(getDuration("2014-02-27T23:00:00.000Z", "2014-03-08T23:00:00.000Z")).to.equal(10);
 		expect(getDuration("2014-06-30T23:00:00.000Z", "2014-07-05T23:00:00.000Z")).to.equal(6);

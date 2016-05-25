338c3d0defb17341d162e8044e1f9306c6e1fed0
Christian H. Mosveen <christian.mosveen@nav.no>
2016-05-25 13:34:03 +0200

    Fikser en bug med html-enkoding ifm tekstbryting av 100 %

--- web/src/frontend/js/components/SykmeldingOpplysning.js
+++ web/src/frontend/js/components/SykmeldingOpplysning.js
@@ -2,7 +2,7 @@ import React, { PropTypes } from 'react';
 
 export const SykmeldingOpplysning = ({ tittel, children, Overskrift = 'H5', className = '' }) => {
     return (<div className={`sykmelding-opplysning ${className}`}>
-        {tittel ? <Overskrift>{tittel}</Overskrift> : ''}
+        {tittel ? <Overskrift dangerouslySetInnerHTML={{ __html: tittel }}></Overskrift> : ''}
         {children}
     </div>);
 };
@@ -16,7 +16,7 @@ SykmeldingOpplysning.propTypes = {
 
 export const SykmeldingNokkelOpplysning = ({ tittel, children, Overskrift = 'H3', className = '' }) => {
     return (<div className={`sykmelding-nokkelopplysning ${className}`}>
-        {tittel ? <Overskrift>{tittel}</Overskrift> : ''}
+        {tittel ? <Overskrift dangerouslySetInnerHTML={{ __html: tittel }}></Overskrift> : ''}
         {children}
     </div>);
 };

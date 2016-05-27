7e796016cb50262296c3f1fd1a6d3a397a7598df
Knut Nygaard <knut.nygaard@nav.no>
2016-05-27 08:53:32 +0200

    [SYFOUTV-234] Lagt inn lenke til sykmeldinger

--- web/src/frontend/js/components/SykmeldingOgOppfolging.js
+++ web/src/frontend/js/components/SykmeldingOgOppfolging.js
@@ -1,8 +1,9 @@
 import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
 import { getLedetekst, getHtmlLedetekst } from '../ledetekster';
 import UnderUtviklingVarselContainer from '../containers/UnderUtviklingVarselContainer.js';
 
-const DineSykmeldinger = ({ ledetekster = {}, skjulVarsel = false }) => {
+const SykmeldingOgOppfolging = ({ ledetekster = {}, skjulVarsel = false }) => {
     return (<div>
         {
             (!skjulVarsel ? <UnderUtviklingVarselContainer/> : '')
@@ -10,12 +11,16 @@ const DineSykmeldinger = ({ ledetekster = {}, skjulVarsel = false }) => {
         <h1 className="side-header typo-sidetittel">
             {getLedetekst('sykmeldingOgOppfolging.sidetittel', ledetekster)}
         </h1>
+        <Link className="dashboard-lenke" to="/sykefravaer/app/sykmeldinger">
+            <img src="/sykefravaer/img/svg/doctor-2.svg" alt="Lege" />
+            <span>Dine sykmeldinger</span>
+        </Link>
     </div>);
 };
 
-DineSykmeldinger.propTypes = {
+SykmeldingOgOppfolging.propTypes = {
     ledetekster: PropTypes.object.isRequired,
     skjulVarsel: PropTypes.bool.isRequired
 };
 
-export default DineSykmeldinger;
+export default SykmeldingOgOppfolging;

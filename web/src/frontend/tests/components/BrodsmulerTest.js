9fb955905433158123a580ff906b8ffd8ba5596f
Helge Fredheim <helge.fredheim@nav.no>
2016-06-23 15:30:13 +0200

    Enklere og kortere brødsmulesti

--- web/src/frontend/tests/components/BrodsmulerTest.js
+++ web/src/frontend/tests/components/BrodsmulerTest.js
@@ -10,6 +10,8 @@ import Brodsmuler from "../../js/components/Brodsmuler.js";
 
 describe("Brodsmuler", () => { 
 
+    let component;
+
     it("Skal vise Ditt NAV dersom ingen brødsmuler sendes inn", function () {
         const brodsmuler =  []
         const component = shallow(<Brodsmuler brodsmuler={brodsmuler} />)
@@ -23,7 +25,8 @@ describe("Brodsmuler", () => {
         }]
         const component = shallow(<Brodsmuler brodsmuler={brodsmuler} />)
         expect(component).to.contain("Ditt NAV");
-        expect(component.find(".js-smule").length).to.equal(2);
+        expect(component.find(".js-smuletekst").length).to.equal(1);
+        expect(component.find(".js-smule").length).to.equal(1);
     });
 
     it("Skal ta hensyn til erKlikkbar-flagget", function () {
@@ -38,5 +41,67 @@ describe("Brodsmuler", () => {
         const component = mount(<Brodsmuler brodsmuler={brodsmuler} />)
         expect(component.find("a").length).to.equal(2);
     });
+
+    it("Skal vise alle brødsmuler dersom det er tre brødsmuler", () => {
+        const brodsmuler =  [{
+            tittel: "Brødsmule 2",
+            erKlikkbar: true,
+            sti: "/brodsmule2"
+        }, {
+            tittel: "Brødsmule 3",
+            erKlikkbar: true,
+            sti: "/brodsmule3"
+        }, {
+            tittel: "Brødsmule 4"
+        }];
+        const component = mount(<Brodsmuler brodsmuler={brodsmuler} />); 
+        expect(component.find("a").length).to.equal(3);
+    }); 
+
+    describe("Dersom det er flere enn tre brødsmuler", () => {
+        beforeEach(() => {
+            const brodsmuler =  [{
+                tittel: "Brødsmule 1", 
+                erKlikkbar: true,
+                sti: "/dine-sykmeldinger"
+            }, {
+                tittel: "Brødsmule 2",
+                erKlikkbar: true,
+                sti: "/brodsmule"
+            }, {
+                tittel: "Brødsmule 3",
+                erKlikkbar: true,
+                sti: "/brodsmule"
+            }, {
+                tittel: "Brødsmule 4",
+                erKlikkbar: true,
+                sti: "/brodsmule"
+            }, {
+                tittel: "Brødsmule 5",
+                erKlikkbar: false,
+            }];
+            component = mount(<Brodsmuler brodsmuler={brodsmuler} />); 
+        }); 
+
+        it("Skal vise en lenke med teksten ...", () => {
+            expect(component.find(".js-toggle").text()).to.equal("...");
+        });
+
+        it("Skal vise de to siste brødsmulene", () => {
+            expect(component.find(".js-smule")).to.have.length(2);
+        });
+
+        it("Skal vise alle dersom man klikker på ...", () => {
+            component.find(".js-toggle").simulate("click");
+            expect(component.find(".js-smule")).to.have.length(5);
+            expect(component.find(".js-toggle")).to.have.length(0);
+        })
+
+    });
+
+
+
+
+
 
 }); 

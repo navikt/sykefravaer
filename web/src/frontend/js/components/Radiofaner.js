64e09e63aff5fb2e332c715e7a6ab6079c149e2d
Helge Fredheim <helge.fredheim@nav.no>
2016-08-09 10:57:30 +0200

    La på ID for hjelpetekst

--- web/src/frontend/js/components/Radiofaner.js
+++ web/src/frontend/js/components/Radiofaner.js
@@ -2,29 +2,31 @@ import React, { PropTypes } from 'react';
 import Hjelpetekst from './skjema/Hjelpetekst';
 
 const Radiofaner = ({ alternativer = [], valgtAlternativ, changeHandler, className, radioName }) => {
-    return (<div className="radiofaner-container"><ul className={`radiofaner ${className}`}>
-        {
-            alternativer.map((a, index) => {
-                const erValgt = a.verdi === valgtAlternativ;
-                const liClassName = a.hjelpetekst ? ' med-hjelpetekst' : '';
-                return (<li className={`nav-input${liClassName}`} key={index}>
-                    <input
-                        type="radio"
-                        className={`nav-radioknapp nav-radioknapp--mork js-${a.verdi}`}
-                        name={radioName}
-                        value={a.verdi}
-                        id={`radio-${a.verdi}`}
-                        checked={erValgt}
-                        onChange={() => {changeHandler(a.verdi);}} />
-                    <label htmlFor={`radio-${a.verdi}`}>{a.tittel}</label>
-                    {
-                        a.hjelpetekst ?
-                        <Hjelpetekst {...a.hjelpetekst} /> : null
-                    }
-                </li>);
-            })
-        }
-    </ul></div>);
+    return (<div className="radiofaner-container">
+        <ul className={`radiofaner ${className}`}>
+            {
+                alternativer.map((a, index) => {
+                    const erValgt = a.verdi === valgtAlternativ;
+                    const liClassName = a.hjelpetekst ? ' med-hjelpetekst' : '';
+                    return (<li className={`nav-input${liClassName}`} key={index}>
+                        <input
+                            type="radio"
+                            className={`nav-radioknapp nav-radioknapp--mork js-${a.verdi}`}
+                            name={radioName}
+                            value={a.verdi}
+                            id={`radio-${a.verdi}`}
+                            checked={erValgt}
+                            onChange={() => {changeHandler(a.verdi);}} />
+                        <label htmlFor={`radio-${a.verdi}`}>{a.tittel}</label>
+                        {
+                            a.hjelpetekst ?
+                            <Hjelpetekst id="velg-arbeidssituasjon" {...a.hjelpetekst} /> : null
+                        }
+                    </li>);
+                })
+            }
+        </ul>
+    </div>);
 };
 
 Radiofaner.propTypes = {

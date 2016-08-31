9a5ab8eb79d231265f42ae5ab0f380c9d54d500d
Helge Fredheim <helge.fredheim@nav.no>
2016-08-31 13:20:19 +0200

    La til et par ID-er for bedre oppsett i Google Tag Manager

--- web/src/frontend/js/components/sykmelding/DinSykmeldingSkjema.js
+++ web/src/frontend/js/components/sykmelding/DinSykmeldingSkjema.js
@@ -112,7 +112,7 @@ class DinSykmeldingSkjema extends Component {
 
         const modus = this.getSkjemaModus();
 
-        return (<form className="panel blokk" onSubmit={handleSubmit(this.handleSubmit)}>
+        return (<form id="dinSykmeldingSkjema" className="panel blokk" onSubmit={handleSubmit(this.handleSubmit)}>
             <h3 className="typo-innholdstittel">Behandle sykmelding</h3>
             <ErOpplysningeneRiktige {...this.props} />
             {
@@ -150,7 +150,7 @@ class DinSykmeldingSkjema extends Component {
                 modus === 'AVBRYT' && <p className="blokk">Når du avbryter sykmeldingen, blablabla...</p>
             }
             <div className="knapperad knapperad-adskilt">
-                <button type="submit" className={`js-submit knapp knapp-hoved ${(sender) ? 'er-inaktiv knapp-spinner js-spinner' : ''}`}>
+                <button type="submit" id="dinSykmeldingSkjemaSubmit" className={`js-submit knapp knapp-hoved ${(sender) ? 'er-inaktiv knapp-spinner js-spinner' : ''}`}>
                     {knappetekster[modus]}
                     <span className="spinner-knapp" />
                 </button>

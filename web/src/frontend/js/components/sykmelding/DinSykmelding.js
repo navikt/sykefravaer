1743bfd3c058b5c040d5215150b509f4ed826fe9
Helge Fredheim <helge.fredheim@nav.no>
2016-09-07 14:56:28 +0200

    La til alt-tekster

--- web/src/frontend/js/components/sykmelding/DinSykmelding.js
+++ web/src/frontend/js/components/sykmelding/DinSykmelding.js
@@ -8,8 +8,8 @@ const DinSykmelding = ({ sykmelding, ledetekster, harPilotarbeidsgiver = false }
         <h1 className="side-header typo-sidetittel">{getLedetekst('din-sykmelding.tittel', ledetekster)}</h1>
         <div className="panel blokk">
             <div className="media">
-                <img src="/sykefravaer/img/svg/nav-ansatt.svg" className="media-img media-img-desktop" alt="" />
-                <img src="/sykefravaer/img/svg/nav-ansatt-mobil.svg" className="media-img media-img-mobil" alt="" />
+                <img src="/sykefravaer/img/svg/nav-ansatt.svg" className="media-img media-img-desktop" alt="Ansatt i NAV" />
+                <img src="/sykefravaer/img/svg/nav-ansatt-mobil.svg" className="media-img media-img-mobil" alt="Ansatt i NAV" />
                 <h2 className="typo-undertittel">{getLedetekst('din-sykmelding.ny-tjeneste.tittel', ledetekster)}</h2>
             </div>
             <div className="redaksjonelt-innhold" dangerouslySetInnerHTML={getHtmlLedetekst('din-sykmelding.ny-tjeneste.tekst', ledetekster)} />

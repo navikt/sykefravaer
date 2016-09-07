1963b6300ac4272fa60b06f2d284e9562c14d7e5
Helge Fredheim <helge.fredheim@nav.no>
2016-09-07 14:54:42 +0200

    Viser 'takk for at du prøver ny tjeneste' osv

--- web/src/frontend/js/components/sykmelding/DinSykmelding.js
+++ web/src/frontend/js/components/sykmelding/DinSykmelding.js
@@ -1,14 +1,24 @@
 import React, { PropTypes } from 'react';
 import DineSykmeldingOpplysninger from '../sykmeldingOpplysninger/DineSykmeldingOpplysninger';
 import DinSykmeldingSkjemaContainer from '../../containers/DinSykmeldingSkjemaContainer';
+import { getLedetekst, getHtmlLedetekst } from '../../ledetekster';
 
 const DinSykmelding = ({ sykmelding, ledetekster, harPilotarbeidsgiver = false }) => {
     return (<div>
+        <h1 className="side-header typo-sidetittel">{getLedetekst('din-sykmelding.tittel', ledetekster)}</h1>
+        <div className="panel blokk">
+            <div className="media">
+                <img src="/sykefravaer/img/svg/nav-ansatt.svg" className="media-img media-img-desktop" alt="" />
+                <img src="/sykefravaer/img/svg/nav-ansatt-mobil.svg" className="media-img media-img-mobil" alt="" />
+                <h2 className="typo-undertittel">{getLedetekst('din-sykmelding.ny-tjeneste.tittel', ledetekster)}</h2>
+            </div>
+            <div className="redaksjonelt-innhold" dangerouslySetInnerHTML={getHtmlLedetekst('din-sykmelding.ny-tjeneste.tekst', ledetekster)} />
+        </div>
         <div className="header-bolk header-sykmelding">
             <img className="header-ikon" src="/sykefravaer/img/svg/person.svg" alt="Du" />
             <img className="header-ikon header-ikon-hoykontrast"
                 src="/sykefravaer/img/svg/person-highcontrast.svg" alt="Du" />
-            <h1 className="header-tittel">{sykmelding.pasient.fornavn} {sykmelding.pasient.etternavn}</h1>
+            <h2 className="header-tittel">{sykmelding.pasient.fornavn} {sykmelding.pasient.etternavn}</h2>
         </div>
         <div className="panel blokk">
             <DineSykmeldingOpplysninger sykmelding={sykmelding} ledetekster={ledetekster} />

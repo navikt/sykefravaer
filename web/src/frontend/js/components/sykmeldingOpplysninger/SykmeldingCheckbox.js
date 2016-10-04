cbdde738b49c66d9f7d7b6191d5078419cc25468
Helge Fredheim <helge.fredheim@nav.no>
2016-10-04 15:16:03 +0200

    Diverse høykontrast-fikser

--- web/src/frontend/js/components/sykmeldingOpplysninger/SykmeldingCheckbox.js
+++ web/src/frontend/js/components/sykmeldingOpplysninger/SykmeldingCheckbox.js
@@ -3,7 +3,7 @@ import React, { PropTypes } from 'react';
 export const SykmeldingCheckbox = ({ tekst, jsClassName, className = '' }) => {
     return (<p className={`checkboxOpplysning ${className} js-${jsClassName}`}>
                 <img src="/sykefravaer/img/png/check-box-1.png" className="ikon" alt="Huket av" />
-                <img src="/sykefravaer/img/png/check-box-1-highcontrast.png" className="ikon-hoykontrast" alt="Huket av" />
+                <img src="/sykefravaer/img/png/check-box-1-highcontrast.png" className="ikon--hoykontrast" alt="Huket av" />
                 <span className="checkboxOpplysning__label" dangerouslySetInnerHTML={{ __html: tekst }}></span>
             </p>);
 };

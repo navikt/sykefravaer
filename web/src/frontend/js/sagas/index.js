634aa3af7aa8c8a2cd01e59a4f29ad473e4d3728
Helge Fredheim <helge.fredheim@nav.no>
2016-11-11 16:18:20 +0100

    La tilbake tidslinjerSagas

--- web/src/frontend/js/sagas/index.js
+++ web/src/frontend/js/sagas/index.js
@@ -3,7 +3,8 @@ import brukerinfoSagas from './brukerinfoSagas';
 import dineArbeidsgivereSagas from './dineArbeidsgivereSagas';
 import dineSykmeldingerSagas from './dineSykmeldingerSagas';
 import dinSykmeldingSagas from './dinSykmeldingSagas';
-import { ledeteksterSagas, tidslinjerSagas } from 'digisyfo-npm';
+import tidslinjerSagas from './tidslinjerSagas';
+import { ledeteksterSagas } from 'digisyfo-npm';
 
 export default function * rootSaga() {
     yield [

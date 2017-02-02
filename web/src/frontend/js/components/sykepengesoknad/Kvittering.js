da9eaad9c9ab25fe29d067477c74b732a759fc28
Helge Fredheim <helge.fredheim@nav.no>
2017-02-02 10:34:56 +0100

    La til kvitteringskomponent. Ikke i bruk p.t.

---
+++ web/src/frontend/js/components/sykepengesoknad/Kvittering.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Sidetopp from '../Sidetopp';
+
+const Kvittering = () => {
+    return (<div>
+        <div className="panel">
+            <h2 className="hode hode-suksess hode-dekorert">Søknaden er sendt til arbeidsgiveren din</h2>
+            <p>Hvis du er heldig, kommer det kanskje noen kroner din vei. Hilsen NAV.</p>
+        </div>
+    </div>)
+}
+
+export default Kvittering;

493b399874777163d6c6a3249a270bd375bb2554
Helge Fredheim <helge.fredheim@nav.no>
2017-02-03 14:47:05 +0100

    Tilbakestilte til stateless component

--- web/src/frontend/js/components/skjema/Tekstfelt.js
+++ web/src/frontend/js/components/skjema/Tekstfelt.js
@@ -1,30 +1,13 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import Feilmelding from './Feilmelding';
 
-class Tekstfelt extends Component {
-    constructor(props) {
-        super(props);
-        this.focus = this.focus.bind(this);
-    }
-
-    focus() {
-        this.textInput.focus();
-    }
-
-    render() {
-        const { meta, className, input, id, placeholder, type } = this.props;
-        return (<div>
-            <input
-                autoComplete="off"
-                placeholder={placeholder}
-                type={type || 'text'}
-                id={id}
-                className={`${className}${meta.touched && meta.error ? ' input--feil' : ''}`}
-                {...input}
-                value={input.value} />
-            <Feilmelding {...meta} />
-        </div>);
-    }
+const Tekstfelt = (props) => {
+    const { meta, className, input, id } = props;
+    return (<div>
+        <input autoComplete="off" placeholder={props.placeholder} type={props.type || 'text'} id={id}
+            className={`${className}${meta.touched && meta.error ? ' input--feil' : ''}`} {...input} value={input.value} />
+        <Feilmelding {...meta} />
+    </div>);
 };
 
 Tekstfelt.propTypes = {

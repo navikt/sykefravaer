6e14085243098ef7c7b5640b186498af673e4c65
Helge Fredheim <helge.fredheim@nav.no>
2017-02-03 09:49:56 +0100

    Rettet 'Vær klar over at...'-tekst på oppsummeringssiden

--- web/src/frontend/js/components/sykepengesoknad/Oppsummering/Oppsummering.js
+++ web/src/frontend/js/components/sykepengesoknad/Oppsummering/Oppsummering.js
@@ -49,9 +49,9 @@ export const OppsummeringWrap = (props) => {
             <div className="panel blokk">
                 <p>Vær klar over at</p>
                 <ul>
-                    <li>rett til sykepenger forutsetter at du har inntektstap på grunn av egen sykdom. Sosiale eller økonomiske problemer gir ikke rett til sykepenger</li>
-                    <li>sykepenger utbetales i maksimum 52 uker, også for gradert (delvis) sykmelding</li>
+                    <li>rett til sykepenger forutsetter at du er borte fra arbeid på grunn av egen sykdom. Sosiale eller økonomiske problemer gir ikke rett til sykepenger</li>
                     <li>du kan miste retten til sykepenger hvis du uten rimelig grunn nekter å opplyse om egen funksjonsevne eller nekter å ta imot tilbud om behandling og/eller tilrettelegging</li>
+                    <li>fristen for å søke sykepenger er som hovedregel 3 måneder</li>
                 </ul>
             </div>
             <Field component={CheckboxSelvstendig} name="bekreftetKorrektInformasjon" id="bekreftetKorrektInformasjon" label={label} />

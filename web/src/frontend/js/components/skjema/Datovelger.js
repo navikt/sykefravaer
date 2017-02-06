6a176329c7b0ae207e99650c413ade2aa6e37ac1
Helge Fredheim <helge.fredheim@nav.no>
2017-02-06 17:07:30 +0100

    Setter inn maske på datofelt

--- web/src/frontend/js/components/skjema/Datovelger.js
+++ web/src/frontend/js/components/skjema/Datovelger.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { parseDato, erGyldigDato } from '../../utils';
 import { Field } from 'redux-form';
-import Tekstfelt from './Tekstfelt';
+import Feilmelding from './Feilmelding';
+import MaskedInput from 'react-maskedinput';
+
+const Datofelt = (props) => {
+    const { meta, className, input, id } = props;
+    return (<div>
+        <MaskedInput
+            type="tel"
+            mask="11.11.1111"
+            autoComplete="off" 
+            placeholder={props.placeholder} type={props.type || 'text'} id={id}
+            className={`input--s ${meta.touched && meta.error ? ' input--feil' : ''}`} {...input} value={input.value} />
+        <Feilmelding {...meta} />
+    </div>);
+};
+
+Datofelt.propTypes = {
+    meta: PropTypes.object,
+    id: PropTypes.string,
+    input: PropTypes.object,
+    type: PropTypes.string,
+    className: PropTypes.string,
+    onKeyUp: PropTypes.func,
+    placeholder: PropTypes.string,
+    mask: PropTypes.string,
+};
 
 const Datovelger = (props) => {
     return (<Field
-        parse={parseDato}
-        component={Tekstfelt}
         placeholder="dd.mm.åååå"
-        className="input--s"
-        type="tel"
+        component={Datofelt}
         validate={(input) => {
             console.log("vallider", input, erGyldigDato(input))
             if (!input) {

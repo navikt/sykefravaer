f0d843acbc91a12c7d549b9843a9f5fb2a9187d8
Helge Fredheim <helge.fredheim@nav.no>
2017-02-06 13:47:24 +0100

    La til tester for Soknad

--- web/src/frontend/js/components/sykepengesoknad/Soknad.js
+++ web/src/frontend/js/components/sykepengesoknad/Soknad.js
@@ -2,7 +2,6 @@ import React, { PropTypes } from 'react';
 import Oppsummering from './Oppsummering/Oppsummering';
 import SykmeldingUtdrag from './SykmeldingUtdrag';
 import { Utvidbar } from 'digisyfo-npm';
-import VaerKlarOverAt from './VaerKlarOverAt';
 
 const Soknad = ({ ledetekster, sykepengesoknad, apentUtdrag = true }) => {
     return (<div>
@@ -11,7 +10,14 @@ const Soknad = ({ ledetekster, sykepengesoknad, apentUtdrag = true }) => {
                 <Oppsummering ledetekster={ledetekster} sykepengesoknad={sykepengesoknad} />
             </Utvidbar>
         </div>
-        <VaerKlarOverAt />
+        <div className="panel blokk js-vaer-klar-over-at">
+            <p>Vær klar over at</p>
+            <ul>
+                <li>rett til sykepenger forutsetter at du er borte fra arbeid på grunn av egen sykdom. Sosiale eller økonomiske problemer gir ikke rett til sykepenger</li>
+                <li>du kan miste retten til sykepenger hvis du uten rimelig grunn nekter å opplyse om egen funksjonsevne eller nekter å ta imot tilbud om behandling og/eller tilrettelegging</li>
+                <li>fristen for å søke sykepenger er som hovedregel 3 måneder</li>
+            </ul>
+        </div>
     </div>);
 };
 

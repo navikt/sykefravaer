c5545099c37d9c5777d826b423177e915e641b52
Helge Fredheim <helge.fredheim@nav.no>
2017-02-07 09:23:38 +0100

    La til punkt i 'Vær klar over at... på oppsummeringssiden

--- web/src/frontend/js/components/sykepengesoknad/Soknad.js
+++ web/src/frontend/js/components/sykepengesoknad/Soknad.js
@@ -15,6 +15,7 @@ const Soknad = ({ ledetekster, sykepengesoknad, apentUtdrag = true }) => {
             <ul>
                 <li>rett til sykepenger forutsetter at du er borte fra arbeid på grunn av egen sykdom. Sosiale eller økonomiske problemer gir ikke rett til sykepenger</li>
                 <li>du kan miste retten til sykepenger hvis du uten rimelig grunn nekter å opplyse om egen funksjonsevne eller nekter å ta imot tilbud om behandling og/eller tilrettelegging</li>
+                <li>sykepenger utbetales i maksimum 52 uker, også for gradert (delvis) sykmelding</li>
                 <li>fristen for å søke sykepenger er som hovedregel 3 måneder</li>
             </ul>
         </div>

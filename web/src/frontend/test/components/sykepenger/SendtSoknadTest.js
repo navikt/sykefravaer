8ddef844710dd0fea8c175515bb4c9061c655b6a
Helge Fredheim <helge.fredheim@nav.no>
2017-02-07 10:06:44 +0100

    La til tester av SendtSoknad

---
+++ web/src/frontend/test/components/sykepenger/SendtSoknadTest.js
@@ -0,0 +1,46 @@
+import chai from 'chai';
+import React from 'react'
+import {mount, shallow, render} from 'enzyme';
+import chaiEnzyme from 'chai-enzyme';
+chai.use(chaiEnzyme());
+const expect = chai.expect;
+import SendtSoknad from '../../../js/components/sykepengesoknad/SendtSoknad';
+import Soknad from '../../../js/components/sykepengesoknad/Soknad';
+import Sidetopp from '../../../js/components/Sidetopp';
+import SykmeldingUtdrag from '../../../js/components/sykepengesoknad/SykmeldingUtdrag';
+import { Avkrysset } from '../../../js/components/sykepengesoknad/Oppsummering/opplysninger';
+import  { getSoknad } from '../../mockSoknader';
+import { Varselstripe } from 'digisyfo-npm';
+
+describe.only("SendtSoknad", () => {
+
+    let component; 
+    let sykepengesoknad = getSoknad();
+    let ledetekster = {"test": "ledetekst" };
+
+    beforeEach(() => {
+        component = shallow(<SendtSoknad sykepengesoknad={sykepengesoknad} ledetekster={ledetekster} />)
+    });
+
+    it("Skal inneholde en Sidetopp", () => {
+        expect(component.contains(<Sidetopp tittel="Søknad om sykepenger" />)).to.be.true;
+    });
+
+    it("Skal inneholde et SykmeldingUtdrag", () => {
+        expect(component.contains(<SykmeldingUtdrag ledetekster={ledetekster} sykepengesoknad={sykepengesoknad} />)).to.be.true;
+    });
+
+    it("Skal inneholde en Soknad", () => {
+        expect(component.contains(<Soknad ledetekster={ledetekster} sykepengesoknad={sykepengesoknad} />)).to.be.true;
+    });
+
+    it("Skal inneholde en Avkrysset", () => {
+        expect(component.contains(<Avkrysset tekst="Jeg har lest all informasjonen jeg har fått i søknaden og bekrefter at opplysningene jeg har gitt er korrekte." />)).to.be.true;
+    });
+
+    it("SKal inneholde en Varselstripe", () => {
+        component = mount(<SendtSoknad sykepengesoknad={sykepengesoknad} ledetekster={ledetekster} />)
+        expect(component.find(Varselstripe)).to.have.length(1);
+    });
+
+})

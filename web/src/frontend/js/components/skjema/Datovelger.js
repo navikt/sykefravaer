e466d95e89013d5ef13d8c82058eebbfe9541e5b
Helge Fredheim <helge.fredheim@nav.no>
2017-02-15 16:32:25 +0100

    Implementerte datovelger med react-daypicker

--- web/src/frontend/js/components/skjema/Datovelger.js
+++ web/src/frontend/js/components/skjema/Datovelger.js
@@ -1,32 +1,20 @@
 import React, { Component, PropTypes } from 'react';
-import { erGyldigDato, erGyldigDatoformat, parseDato, fraInputdatoTilJSDato } from '../../utils';
+import { erGyldigDato, erGyldigDatoformat } from '../../utils';
 import { Field } from 'redux-form';
 import Feilmelding from './Feilmelding';
-import DayPicker, { DateUtils } from 'react-day-picker';
 import { connect } from 'react-redux';
-import { autofill } from 'redux-form';
 import { SYKEPENGER_SKJEMANAVN } from '../sykepengesoknad/setup';
-import { toDatePrettyPrint } from 'digisyfo-npm';
 import MaskedInput from 'react-maskedinput';
+import { toDatePrettyPrint } from 'digisyfo-npm';
+import DayPickerComponent from './DayPicker';
+import { autofill } from 'redux-form';
 
-const MONTHS = ['januar', 'februar', 'mars', 'april', 'mai', 'juni', 'juli', 'august', 'september', 'oktober', 'november', 'desember'];
-const WEEKDAYS_LONG = ['mandag', 'tirsdag', 'onsdag', 'torsdag', 'fredag', 'lørdag', 'søndag'];
-const WEEKDAYS_SHORT = ['ma', 'ti', 'on', 'to', 'fr', 'lø', 'sø'];
-
-class Datofelt extends Component {
+export class DatoField extends Component {
     constructor(props) {
         super(props);
         this.state = {
             erApen: false,
-            valgtDato: new Date(),
         };
-    }
-
-    onKeyUp(e) {
-        if (e.which === 27) {
-            // Escape
-            this.lukk();
-        }
     }
 
     toggle() {
@@ -50,106 +38,87 @@ class Datofelt extends Component {
         this.refs.toggle.focus();
     }
 
-    getDateFromValue () {
-        const { input } = this.props;
-        const v = input.value;
-        if (!erGyldigDatoformat(v) || !erGyldigDato(v)) {
-            return undefined;
+    onKeyUp(e) {
+        if (e.which === 27) {
+            this.lukk();
         }
-        else {
-            const d = input.value.split('.');
-            return new Date(`${d[2]}-${d[1]}-${d[0]}`);            
-        }
-    }
-
-    getInitialMonth() {
-        const s = this.getDateFromValue();
-        if (s) {
-            return s;
-        }
-        return new Date();
     }
 
     render() {
-        const { meta, input, id, dispatch } = this.props;
-        return (<div>
-            <div>
-                <MaskedInput
-                    type="tel"
-                    mask="11.11.1111"
-                    autoComplete="off" 
-                    placeholder="dd.mm.åååå"
-                    id={id}
-                    className={`input--dato ${meta.touched && meta.error ? ' input--feil' : ''}`} {...input} />
-                <button ref="toggle" 
+        const { meta, input, id } = this.props;
+
+        return (<div className="datovelger">
+            <div className="datovelger__inner" onClick={(e) => {
+                e.nativeEvent && e.nativeEvent.stopImmediatePropagation ? e.nativeEvent.stopImmediatePropagation() : e.stopPropagation();
+            }}>
+                <div className="datovelger__inputContainer">
+                    <MaskedInput
+                        type="tel"
+                        mask="11.11.1111"
+                        autoComplete="off" 
+                        placeholder="dd.mm.åååå"
+                        id={id}
+                        className={`datovelger__input${meta.touched && meta.error ? ' input--feil' : ''}`} {...input} />
+                    <button 
+                        className="js-toggle datovelger__toggleDayPicker"
+                        ref="toggle"
+                        id={`toggle-${id}`}
+                        onKeyUp={(e) => {
+                            this.onKeyUp(e);
+                        }}
+                        onClick={(e) => {
+                            e.preventDefault();
+                            this.toggle();
+                        }}
+                        aria-pressed={this.erApen}>
+                        {this.state.erApen ? "Skjul datovelger" : "Vis datovelger"}
+                    </button>
+                </div>
+                { this.state.erApen && <DayPickerComponent
+                    {...this.props}
+                    ariaControlledBy={`toggle-${id}`}
+                    onDayClick={(event, jsDato) => {
+                        const { dispatch, input, skjemanavn } = this.props;
+                        var s = toDatePrettyPrint(new Date(jsDato));
+                        dispatch(autofill(skjemanavn, input.name, s));
+                        this.lukk();
+                    }}
                     onKeyUp={(e) => {
                         this.onKeyUp(e);
                     }}
-                    onClick={(e) => {
-                        e.preventDefault();
-                        this.toggle();
-                    }}
-                    aria-pressed={this.erApen}>
-                    {this.state.erApen ? "Skjul datovelger" : "Vis datovelger"}
-                </button>
-            </div>
-            { this.state.erApen && <div className="DayPicker__Wrapper" onKeyUp={(e) => {
-                this.onKeyUp(e);
-            }}>
-                <DayPicker
-                    ref={(el) => {
-                        this.daypicker = el;
-                    }}
-                    locale="no"
-                    months={MONTHS}
-                    weekdaysLong={WEEKDAYS_LONG}
-                    weekdaysShort={WEEKDAYS_SHORT}
-                    initialMonth={this.getInitialMonth()}
-                    selectedDays={(day) => {
-                        if (!this.getDateFromValue()) {
-                            return false;
-                        } else {
-                            return DateUtils.isSameDay(this.getDateFromValue(), day)
-                        }
-                    }}
-                    onDayClick={(event, jsDato) => {
-                        var s = toDatePrettyPrint(new Date(jsDato));
-                        dispatch(autofill(SYKEPENGER_SKJEMANAVN, input.name, s));
+                    lukk={() => {
                         this.lukk();
-                    }}
-                />
-            </div>}
-            <Feilmelding {...meta} />
+                    }} />}
+                <Feilmelding {...meta} />
+            </div>
         </div>);
     }
 }
 
-Datofelt.propTypes = {
+DatoField.propTypes = {
     meta: PropTypes.object,
     id: PropTypes.string,
     input: PropTypes.object,
-    type: PropTypes.string,
-    onKeyUp: PropTypes.func,
-    placeholder: PropTypes.string,
-    mask: PropTypes.string,
 };
 
-Datofelt = connect()(Datofelt)
+const ConnectedDatoField = connect()(DatoField)
+
+export const validerDatoField = (input) => {
+    if (!input) {
+        return undefined;
+    } else if (!erGyldigDatoformat(input)) {
+        return 'Datoen må være på formatet dd.mm.åååå';
+    } else if (!erGyldigDato(input)) {
+        return 'Datoen er ikke gyldig';
+    }
+    return undefined;
+};
 
 let Datovelger = (props) => {
     return (<Field
-        placeholder="dd.mm.åååå"
-        component={Datofelt}
-        validate={(input) => {
-            if (!input) {
-                return undefined;
-            } else if (!erGyldigDatoformat(input)) {
-                return 'Datoen må være på formatet dd.mm.åååå';
-            } else if (!erGyldigDato(input)) {
-                return 'Datoen er ikke gyldig';
-            }
-            return undefined;
-        }}
+        component={ConnectedDatoField}
+        skjemanavn={SYKEPENGER_SKJEMANAVN}
+        validate={validerDatoField}
         {...props} />);
 };
 
---
+++ web/src/frontend/js/components/skjema/DayPicker.js
@@ -0,0 +1,98 @@
+import React, { Component, PropTypes } from 'react';
+import { erGyldigDato, erGyldigDatoformat } from '../../utils';
+import DayPicker, { DateUtils, LocaleUtils } from 'react-day-picker';
+
+export const MONTHS = ['januar', 'februar', 'mars', 'april', 'mai', 'juni', 'juli', 'august', 'september', 'oktober', 'november', 'desember'];
+export const WEEKDAYS_LONG = ['søndag', 'mandag', 'tirsdag', 'onsdag', 'torsdag', 'fredag', 'lørdag'];
+export const WEEKDAYS_SHORT = ['søn', 'man', 'tir', 'ons', 'tor', 'fre', 'lør'];
+
+export const formatDay = (date) => {
+    // aria-label på dager
+    return `${WEEKDAYS_LONG[date.getDay()]} ${date.getDate()}. ${MONTHS[date.getMonth()]} ${date.getFullYear()}`;
+};
+
+const localeUtils = Object.assign({}, LocaleUtils, {
+    formatDay,
+});
+
+export const Caption = ({ date }) => {
+    return (<div className="DayPicker-Caption" role="heading" aria-live="assertive" aria-atomic="true">
+        {`${MONTHS[date.getMonth()]} ${date.getFullYear()}`}
+    </div>);
+}
+
+let lukk;
+
+class DayPickerComponent extends Component {
+    componentDidMount() {
+        lukk = () => {
+            this.props.lukk();
+        }
+        document.addEventListener('click', lukk);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('click', lukk);
+    }
+
+    getDateFromValue () {
+        const { input } = this.props;
+        const v = input.value;
+        if (!erGyldigDatoformat(v) || !erGyldigDato(v)) {
+            return undefined;
+        }
+        else {
+            const d = input.value.split('.');
+            return new Date(`${d[2]}-${d[1]}-${d[0]}`);            
+        }
+    }
+
+    getInitialMonth() {
+        const s = this.getDateFromValue();
+        if (s) {
+            return s;
+        }
+        return new Date();
+    }
+
+    selectedDays(day) {
+        if (!this.getDateFromValue()) {
+            return false;
+        } else {
+            return DateUtils.isSameDay(this.getDateFromValue(), day)
+        }
+    }
+
+    render() {
+        const { ariaControlledBy, onKeyUp } = this.props;
+        return (<div className="datovelger__DayPicker"
+            aria-controlledby={ariaControlledBy}
+            onKeyUp={(e) => {
+                onKeyUp(e);
+            }}>
+            <DayPicker
+                locale="no"
+                months={MONTHS}
+                weekdaysLong={WEEKDAYS_LONG}
+                weekdaysShort={WEEKDAYS_SHORT}
+                initialMonth={this.getInitialMonth()}
+                localeUtils={localeUtils}
+                firstDayOfWeek={1}
+                captionElement={<Caption />}
+                selectedDays={(day) => {
+                    return this.selectedDays(day);
+                }}
+                onDayClick={(event, jsDato) => {
+                    this.props.onDayClick(event, jsDato);
+                }}
+            />
+        </div>)
+    }
+}
+
+DayPickerComponent.propTypes = {
+    input: PropTypes.object,
+    onKeyUp: PropTypes.func.isRequired,
+};
+
+export default DayPickerComponent;

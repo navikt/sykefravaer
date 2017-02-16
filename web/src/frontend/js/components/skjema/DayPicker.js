2c062db7f745852735097876823a828cab4ce909
Helge Fredheim <helge.fredheim@nav.no>
2017-02-16 11:53:08 +0100

    Laget custom navbar for å få til  tekst Forrige/Neste på forrige/neste-knapper

--- web/src/frontend/js/components/skjema/DayPicker.js
+++ web/src/frontend/js/components/skjema/DayPicker.js
@@ -25,6 +25,36 @@ Caption.propTypes = {
     date: PropTypes.instanceOf(Date),
 };
 
+export const NavBar = ({ onNextClick, onPreviousClick, showPreviousButton, showNextButton }) => {
+    const className = 'DayPicker-NavButton';
+    return (<div role="toolbar">
+        <button
+            tabIndex="-1"
+            className={`${className} DayPicker-NavButton--prev`}
+            disabled={!showPreviousButton}
+            type="button" onClick={(e) => {
+                e.preventDefault();
+                onPreviousClick();
+            }}>Forrige måned</button>
+        <button
+            tabIndex="-1"
+            className={`${className} DayPicker-NavButton--next`}
+            disabled={!showNextButton}
+            type="button"
+            onClick={(e) => {
+                e.preventDefault();
+                onNextClick();
+            }}>Neste måned</button>
+    </div>);
+};
+
+NavBar.propTypes = {
+    onNextClick: PropTypes.func,
+    onPreviousClick: PropTypes.func,
+    showPreviousButton: PropTypes.bool,
+    showNextButton: PropTypes.bool,
+};
+
 let lukk;
 
 class DayPickerComponent extends Component {
@@ -80,6 +110,7 @@ class DayPickerComponent extends Component {
                 localeUtils={localeUtils}
                 firstDayOfWeek={1}
                 captionElement={<Caption />}
+                navbarElement={<NavBar />}
                 selectedDays={(day) => {
                     return this.selectedDays(day);
                 }}
--- web/src/frontend/js/components/sykepengesoknad/FoerDuBegynner/BekreftAnsvar.js
+++ web/src/frontend/js/components/sykepengesoknad/FoerDuBegynner/BekreftAnsvar.js
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 import { Field } from 'redux-form';
 import CheckboxSelvstendig from '../../skjema/CheckboxSelvstendig';
+import Datovelger from '../../skjema/Datovelger';
 import { getLedetekst } from 'digisyfo-npm';
 
 const BekreftAnsvar = ({ ledetekster }) => {
@@ -8,6 +9,8 @@ const BekreftAnsvar = ({ ledetekster }) => {
     return (<div className="blokk">
         <p>{getLedetekst('sykepengesoknad.bekreft-ansvar.introtekst', ledetekster)}</p>
         <Field component={CheckboxSelvstendig} name="ansvarBekreftet" id="ansvarBekreftet" label={label} />
+        <Datovelger name="test" id="hai" />
+        <Datovelger name="test2" id="hai2" />
     </div>);
 };
 

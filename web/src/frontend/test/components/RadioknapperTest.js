56bffeb11a8e9677066041afe8e78251bee1983b
Knut Nygaard <knut.nygaard@nav.no>
2017-02-17 11:52:48 +0100

    Legger til hjelptekst på spørsmål om egenmeldingsdager

---
+++ web/src/frontend/test/components/RadioknapperTest.js
@@ -0,0 +1,25 @@
+import chai from "chai";
+import React from "react";
+import {mount, shallow} from "enzyme";
+import chaiEnzyme from "chai-enzyme";
+import Radioknapper from "../../js/components/skjema/Radioknapper";
+import {Hjelpetekst} from "digisyfo-npm";
+
+chai.use(chaiEnzyme());
+const expect = chai.expect;
+
+describe("Radioknapper", () => {
+
+    it("Viser hjelpetekst om satt", () => {
+        const hjelpetekst = <Hjelpetekst id={'1'} tekst={'tekst'} tittel={'tittel'}/>;
+        const comp = shallow(<Radioknapper hjelpetekst={hjelpetekst} children={[]} ledetekster={{}} meta={{}} spoersmal='spoersmaal' />)
+        expect(comp.find(Hjelpetekst)).to.have.length(1);
+    });
+
+    it("Viser ikke hjelpetekst om satt", () => {
+        const hjelpetekst = <Hjelpetekst id={'1'} tekst={'tekst'} tittel={'tittel'}/>;
+        const comp = shallow(<Radioknapper hjelpetekst={hjelpetekst} children={[]} ledetekster={{}} meta={{}} spoersmal='spoersmaal' />)
+        expect(comp.find(Hjelpetekst)).to.have.length(1);
+    });
+});
+

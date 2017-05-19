2f0dd763a9ea98b66ed8aec46253ed676f4ed50d
Erik Jansen <erik.gunnar.jansen@nav.no>
2017-05-19 15:23:08 +0200

    Endre logo for landinslenke
    SYFOUTV-1019

--- web/src/frontend/js/components/landingsside/Landingsside.js
+++ web/src/frontend/js/components/landingsside/Landingsside.js
@@ -60,7 +60,7 @@ const Landingsside = ({ skjulVarsel = false, sykepengesoknader = [], harDialogmo
             }
 
             {
-                <LandingssideLenke to="/sykefravaer/oppfolgingsdialoger" ikon="oppfolgingsdialoger" ikonAlt="Oppfølgingsdialoger" tittel="Oppfølgingsdialoger" variant="koromiko" />
+                <LandingssideLenke to="/sykefravaer/oppfolgingsdialoger" ikon="oppfolgingsplaner" ikonAlt="Oppfølgingsplaner" tittel="Oppfølgingsplaner" variant="koromiko" />
             }
         </nav>
         <NaermesteLedereContainer />

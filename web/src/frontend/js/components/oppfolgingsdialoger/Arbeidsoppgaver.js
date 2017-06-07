e6f080ca4d173f9eab998fe8aa8df4b219d2a1ef
Erik Jansen <erik.gunnar.jansen@nav.no>
2017-06-07 16:40:18 +0200

    Refaktorer ArbeidsoppgaveContainer
    SYFOUTV-1027

---
+++ web/src/frontend/js/components/oppfolgingsdialoger/Arbeidsoppgaver.js
@@ -0,0 +1,126 @@
+import React, { Component, PropTypes } from 'react';
+import { isEmpty } from '../../utils/oppfolgingsdialogUtils';
+import {
+    OppfolgingsdialogSide,
+    OppfolgingsdialogOppgaveTabell,
+    finnArbeidsoppgaverIkkeVurdertAvSykmeldt,
+    NotifikasjonBoks,
+} from 'oppfolgingsdialog-npm';
+import { getLedetekst } from 'digisyfo-npm';
+import LagreArbeidsoppgaveSkjema from './LagreArbeidsoppgaveSkjema';
+
+export const RenderNotifikasjonBoks = ({ virksomhetsnavn, antallIkkeVurderteArbeidsoppgaver }) => {
+    return (<NotifikasjonBoks
+        imgUrl={"/sykefravaer/img/svg/notifikasjon-illustrasjon.svg"}
+        tekst={getLedetekst('oppfolgingsdialog.notifikasjonboks.ikke-vurderte-arbeidsoppgaver.tekst', {
+            '%VIRKSOMHETSNAVN%': virksomhetsnavn,
+            '%ANTALLARBEIDSOPPGAVER%': antallIkkeVurderteArbeidsoppgaver.toString(),
+        })}
+        classNames={'panel--oransje'}
+    />);
+};
+RenderNotifikasjonBoks.propTypes = {
+    virksomhetsnavn: PropTypes.string,
+    antallIkkeVurderteArbeidsoppgaver: PropTypes.number,
+};
+
+export const RenderOppfolgingsdialogOppgaveTabell = ({ arbeidsoppgaveListe }) => {
+    return (
+        <OppfolgingsdialogOppgaveTabell
+            arbeidsoppgaveListe={arbeidsoppgaveListe}
+            urlImgArrow="/sykefravaer/img/svg/arrow-down.svg"
+            urlImgVarsel="/sykefravaer/img/svg/varseltrekant.svg"
+        />
+    );
+};
+RenderOppfolgingsdialogOppgaveTabell.propTypes = {
+    arbeidsoppgaveListe: PropTypes.array,
+};
+
+export const RenderKnapper = ({ toggleArbeidsoppgaveSkjema }) => {
+    return (
+        <div className="knapperad">
+            <button
+                className="knapp knapperad__element"
+                onClick={toggleArbeidsoppgaveSkjema}>
+                {getLedetekst('oppfolgingsdialog.arbeidstaker.knapp.leggtil-arbeidsoppgave')}
+            </button>
+        </div>
+    );
+};
+RenderKnapper.propTypes = {
+    toggleArbeidsoppgaveSkjema: PropTypes.func,
+};
+
+export class Arbeidsoppgaver extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            visArbeidsoppgaveSkjema: false,
+        };
+        this.toggleArbeidsoppgaveSkjema = this.toggleArbeidsoppgaveSkjema.bind(this);
+    }
+
+    toggleArbeidsoppgaveSkjema() {
+        this.setState({
+            visArbeidsoppgaveSkjema: !this.state.visArbeidsoppgaveSkjema,
+        });
+    }
+
+    render() {
+        const { ledetekster, oppfolgingsdialog, oppfolgingsdialogId, sendArbeidsoppgave } = this.props;
+
+        const antallIkkeVurderteArbeidsoppgaver = oppfolgingsdialog ? finnArbeidsoppgaverIkkeVurdertAvSykmeldt(oppfolgingsdialog.arbeidsoppgaveListe).length : 0;
+
+        return (
+            <OppfolgingsdialogSide
+                brukernavn={oppfolgingsdialog.virksomhetsnavn}
+                ledetekster={ledetekster}
+                rootUrl={`/sykefravaer/oppfolgingsplaner/${oppfolgingsdialogId}`}>
+                {
+                    isEmpty(oppfolgingsdialog.arbeidsoppgaveListe) ?
+                        <div>
+                            <h2 className="typo-undertittel">{getLedetekst('oppfolgingsdialog.arbeidstaker.arbeidsoppgave.opprett.tittel')}</h2>
+                            <LagreArbeidsoppgaveSkjema
+                                avbrytHref={'/sykefravaer/oppfolgingsplaner'}
+                                sendArbeidsoppgave={sendArbeidsoppgave}
+                            />
+                        </div>
+                        :
+                        <div>
+                            <h2 className="typo-undertittel">{getLedetekst('oppfolgingsdialog.arbeidstaker.arbeidsoppgave.opprett.tittel')}</h2>
+                            {
+                                antallIkkeVurderteArbeidsoppgaver > 0 &&
+                                <RenderNotifikasjonBoks
+                                    virksomhetsnavn={oppfolgingsdialog.virksomhetsnavn}
+                                    antallIkkeVurderteArbeidsoppgaver={antallIkkeVurderteArbeidsoppgaver} />
+                            }
+                            {
+                                <RenderOppfolgingsdialogOppgaveTabell
+                                    arbeidsoppgaveListe={oppfolgingsdialog.arbeidsoppgaveListe} />
+                            }
+                            {
+                                this.state.visArbeidsoppgaveSkjema ?
+                                    <LagreArbeidsoppgaveSkjema
+                                        avbrytHref={`/sykefravaer/oppfolgingsplaner/${oppfolgingsdialogId}/arbeidsoppgaver`}
+                                        sendArbeidsoppgave={this.sendArbeidsoppgave}
+                                        cancel={this.toggleArbeidsoppgaveSkjema}
+                                    /> :
+                                    <RenderKnapper toggleArbeidsoppgaveSkjema={this.toggleArbeidsoppgaveSkjema} />
+                            }
+                        </div>
+                }
+            </OppfolgingsdialogSide>
+        );
+    }
+}
+
+Arbeidsoppgaver.propTypes = {
+    ledetekster: PropTypes.object,
+    oppfolgingsdialog: PropTypes.object,
+    oppfolgingsdialogId: PropTypes.string,
+    sendArbeidsoppgave: PropTypes.func,
+};
+
+export default Arbeidsoppgaver;
--- web/src/frontend/js/containers/ArbeidsoppgaverContainer.js
+++ web/src/frontend/js/containers/ArbeidsoppgaverContainer.js
@@ -3,70 +3,19 @@ import { connect } from 'react-redux';
 import Side from '../sider/Side';
 import AppSpinner from '../components/AppSpinner';
 import Feilmelding from '../components/Feilmelding';
-import { getOppfolgingsdialog, isEmpty } from '../utils/oppfolgingsdialogUtils';
+import { getOppfolgingsdialog } from '../utils/oppfolgingsdialogUtils';
 import { hentOppfolgingsdialogerAt as hentOppfolgingsdialoger } from 'oppfolgingsdialog-npm';
 import {
     lagreArbeidsoppgave,
-    OppfolgingsdialogSide,
-    OppfolgingsdialogOppgaveTabell,
-    finnArbeidsoppgaverIkkeVurdertAvSykmeldt,
-    NotifikasjonBoks,
 } from 'oppfolgingsdialog-npm';
 import { getLedetekst } from 'digisyfo-npm';
 import { brodsmule as brodsmulePt } from '../propTypes';
-import LagreArbeidsoppgaveSkjema from '../components/oppfolgingsdialoger/LagreArbeidsoppgaveSkjema';
-
-export const RenderNotifikasjonBoks = ({ virksomhetsnavn, antallIkkeVurderteArbeidsoppgaver }) => {
-    return (<NotifikasjonBoks
-        imgUrl={"/sykefravaer/img/svg/notifikasjon-illustrasjon.svg"}
-        tekst={getLedetekst('oppfolgingsdialog.notifikasjonboks.ikke-vurderte-arbeidsoppgaver.tekst', {
-            '%VIRKSOMHETSNAVN%': virksomhetsnavn,
-            '%ANTALLARBEIDSOPPGAVER%': antallIkkeVurderteArbeidsoppgaver.toString(),
-        })}
-        classNames={'panel--oransje'}
-    />);
-};
-RenderNotifikasjonBoks.propTypes = {
-    virksomhetsnavn: PropTypes.string,
-    antallIkkeVurderteArbeidsoppgaver: PropTypes.number,
-};
-
-export const RenderOppfolgingsdialogOppgaveTabell = ({ arbeidsoppgaveListe }) => {
-    return (
-        <OppfolgingsdialogOppgaveTabell
-            arbeidsoppgaveListe={arbeidsoppgaveListe}
-            urlImgArrow="/sykefravaer/img/svg/arrow-down.svg"
-            urlImgVarsel="/sykefravaer/img/svg/varseltrekant.svg"
-        />
-    );
-};
-RenderOppfolgingsdialogOppgaveTabell.propTypes = {
-    arbeidsoppgaveListe: PropTypes.array,
-};
-
-export const RenderKnapper = ({ toggleArbeidsoppgaveSkjema }) => {
-    return (
-        <div className="knapperad">
-            <button
-                className="knapp knapperad__element"
-                onClick={toggleArbeidsoppgaveSkjema}>
-                {getLedetekst('oppfolgingsdialog.arbeidstaker.knapp.leggtil-arbeidsoppgave')}
-            </button>
-        </div>
-    );
-};
-RenderKnapper.propTypes = {
-    toggleArbeidsoppgaveSkjema: PropTypes.func,
-};
+import Arbeidsoppgaver from '../components/oppfolgingsdialoger/Arbeidsoppgaver';
 
 export class ArbeidsoppgaverSide extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            visArbeidsoppgaveSkjema: false,
-        };
-        this.toggleArbeidsoppgaveSkjema = this.toggleArbeidsoppgaveSkjema.bind(this);
         this.sendArbeidsoppgave = this.sendArbeidsoppgave.bind(this);
     }
 
@@ -76,20 +25,12 @@ export class ArbeidsoppgaverSide extends Component {
         }
     }
 
-    toggleArbeidsoppgaveSkjema() {
-        this.setState({
-            visArbeidsoppgaveSkjema: !this.state.visArbeidsoppgaveSkjema,
-        });
-    }
-
     sendArbeidsoppgave(values) {
         this.props.lagreArbeidsoppgave(this.props.oppfolgingsdialogId, values);
     }
 
     render() {
         const { brodsmuler, ledetekster, oppfolgingsdialog, oppfolgingsdialogId, henter, hentingFeilet, lagrer, lagringFeilet } = this.props;
-
-        const antallIkkeVurderteArbeidsoppgaver = oppfolgingsdialog ? finnArbeidsoppgaverIkkeVurdertAvSykmeldt(oppfolgingsdialog.arbeidsoppgaveListe).length : 0;
 
         return (<Side tittel={getLedetekst('oppfolgingsdialog.sidetittel')} brodsmuler={brodsmuler}>
             { (() => {
@@ -99,44 +40,12 @@ export class ArbeidsoppgaverSide extends Component {
                     return (<Feilmelding />);
                 }
                 return (
-                    <OppfolgingsdialogSide
-                        brukernavn={oppfolgingsdialog.virksomhetsnavn}
+                    <Arbeidsoppgaver
+                        oppfolgingsdialog={oppfolgingsdialog}
                         ledetekster={ledetekster}
-                        rootUrl={`/sykefravaer/oppfolgingsplaner/${oppfolgingsdialogId}`}>
-                        {
-                            isEmpty(oppfolgingsdialog.arbeidsoppgaveListe) ?
-                                <div>
-                                    <h2 className="typo-undertittel">{getLedetekst('oppfolgingsdialog.arbeidstaker.arbeidsoppgave.opprett.tittel')}</h2>
-                                    <LagreArbeidsoppgaveSkjema
-                                        avbrytHref={'/sykefravaer/oppfolgingsplaner'}
-                                        sendArbeidsoppgave={this.sendArbeidsoppgave}
-                                    />
-                                </div>
-                                :
-                                <div>
-                                    <h2 className="typo-undertittel">{getLedetekst('oppfolgingsdialog.arbeidstaker.arbeidsoppgave.opprett.tittel')}</h2>
-                                    {
-                                        antallIkkeVurderteArbeidsoppgaver > 0 &&
-                                        <RenderNotifikasjonBoks
-                                            virksomhetsnavn={oppfolgingsdialog.virksomhetsnavn}
-                                            antallIkkeVurderteArbeidsoppgaver={antallIkkeVurderteArbeidsoppgaver} />
-                                    }
-                                    {
-                                        <RenderOppfolgingsdialogOppgaveTabell
-                                            arbeidsoppgaveListe={oppfolgingsdialog.arbeidsoppgaveListe} />
-                                    }
-                                    {
-                                        this.state.visArbeidsoppgaveSkjema ?
-                                            <LagreArbeidsoppgaveSkjema
-                                                avbrytHref={`/sykefravaer/oppfolgingsplaner/${oppfolgingsdialogId}/arbeidsoppgaver`}
-                                                sendArbeidsoppgave={this.sendArbeidsoppgave}
-                                                cancel={this.toggleArbeidsoppgaveSkjema}
-                                            /> :
-                                            <RenderKnapper toggleArbeidsoppgaveSkjema={this.toggleArbeidsoppgaveSkjema} />
-                                    }
-                                </div>
-                        }
-                    </OppfolgingsdialogSide>
+                        oppfolgingsdialogId={oppfolgingsdialogId}
+                        sendArbeidsoppgave={this.sendArbeidsoppgave}
+                    />
                 );
             })()
             }

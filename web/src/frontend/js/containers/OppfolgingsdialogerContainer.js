66caa6fdd4d83b2f4af395f1fe14d7d2e5557ea7
Erik Jansen <erik.gunnar.jansen@nav.no>
2017-06-13 09:24:12 +0200

    Flytt dispatch hentOppDialoger til Container

--- web/src/frontend/js/containers/OppfolgingsdialogerContainer.js
+++ web/src/frontend/js/containers/OppfolgingsdialogerContainer.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { getLedetekst } from 'digisyfo-npm';
 import Side from '../sider/Side';
@@ -6,37 +6,50 @@ import Feilmelding from '../components/Feilmelding';
 import AppSpinner from '../components/AppSpinner';
 import { brodsmule as brodsmulePt } from '../propTypes';
 import Oppfolgingsdialoger from '../components/oppfolgingsdialoger/Oppfolgingsdialoger';
-import { OppfolgingsdialogInfoboks } from 'oppfolgingsdialog-npm';
+import { OppfolgingsdialogInfoboks, hentOppfolgingsdialogerAt as hentOppfolgingsdialoger } from 'oppfolgingsdialog-npm';
 
-export const OppfolgingsdialogerSide = ({ brodsmuler, oppfolgingsdialoger, ledetekster, henter, hentingFeilet, brukerHarTilgang }) => {
-    return (<Side tittel={getLedetekst('oppfolgingsdialoger.sidetittel', ledetekster)} brodsmuler={brodsmuler}>
-        {
-            (() => {
-                if (henter) {
-                    return <AppSpinner />;
-                } else if (hentingFeilet) {
-                    return (<Feilmelding />);
-                } else if (brukerHarTilgang) {
-                    return (<Oppfolgingsdialoger
-                        oppfolgingsdialoger={oppfolgingsdialoger}
-                        ledetekster={ledetekster}
-                    />);
-                }
-                return (<OppfolgingsdialogInfoboks
-                    svgUrl="/sykefravaer/img/svg/oppfolgingsdialog-infoboks-ikkeTilgang.svg"
-                    svgAlt="ikkeTilgang"
-                    tittel={getLedetekst('oppfolgingsdialog.infoboks.ikke-tilgang.tittel')}
-                    tekst={getLedetekst('oppfolgingsdialog.infoboks.ikke-tilgang.kodebegrensning.tekst')}
-                    />
-                );
-            })()
+export class OppfolgingsdialogerSide extends Component {
+    componentWillMount() {
+        const { oppfolgingsdialogerHentet } = this.props;
+        if (!oppfolgingsdialogerHentet) {
+            this.props.hentOppfolgingsdialoger();
         }
-    </Side>);
+    }
+
+    render() {
+        const {brodsmuler, oppfolgingsdialoger, ledetekster, henter, hentingFeilet, brukerHarTilgang} = this.props;
+
+        return (<Side tittel={getLedetekst('oppfolgingsdialoger.sidetittel', ledetekster)} brodsmuler={brodsmuler}>
+            {
+                (() => {
+                    if (henter) {
+                        return <AppSpinner />;
+                    } else if (hentingFeilet) {
+                        return (<Feilmelding />);
+                    } else if (brukerHarTilgang) {
+                        return (<Oppfolgingsdialoger
+                            oppfolgingsdialoger={oppfolgingsdialoger}
+                            ledetekster={ledetekster}
+                        />);
+                    }
+                    return (<OppfolgingsdialogInfoboks
+                            svgUrl="/sykefravaer/img/svg/oppfolgingsdialog-infoboks-ikkeTilgang.svg"
+                            svgAlt="ikkeTilgang"
+                            tittel={getLedetekst('oppfolgingsdialog.infoboks.ikke-tilgang.tittel')}
+                            tekst={getLedetekst('oppfolgingsdialog.infoboks.ikke-tilgang.kodebegrensning.tekst')}
+                        />
+                    );
+                })()
+            }
+        </Side>);
+    }
 };
 
 OppfolgingsdialogerSide.propTypes = {
     brodsmuler: PropTypes.arrayOf(brodsmulePt),
     oppfolgingsdialoger: PropTypes.array,
+    hentOppfolgingsdialoger: PropTypes.func,
+    oppfolgingsdialogerHenter: PropTypes.bool,
     ledetekster: PropTypes.object,
     henter: PropTypes.bool,
     hentingFeilet: PropTypes.bool,
@@ -49,6 +62,7 @@ export const mapStateToProps = (state) => {
     return {
         ledetekster: state.ledetekster.data,
         oppfolgingsdialoger: state.oppfolgingsdialoger.data,
+        oppfolgingsdialogerHenter: state.oppfolgingsdialoger.henter,
         henter: state.ledetekster.henter || state.oppfolgingsdialoger.henter,
         hentingFeilet: state.ledetekster.hentingFeilet || state.oppfolgingsdialoger.hentingFeilet,
         brodsmuler: [{
@@ -63,6 +77,6 @@ export const mapStateToProps = (state) => {
     };
 };
 
-const OppfolgingsdialogerContainer = connect(mapStateToProps)(OppfolgingsdialogerSide);
+const OppfolgingsdialogerContainer = connect(mapStateToProps, { hentOppfolgingsdialoger })(OppfolgingsdialogerSide);
 
 export default OppfolgingsdialogerContainer;

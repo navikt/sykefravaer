353420ece94566c65d1dd1655b6f83a2f4db8789
Erik Jansen <erik.gunnar.jansen@nav.no>
2017-07-06 14:21:05 +0200

    Refaktor: GodkjePlann ut i egen komponent fra Plan
    SYFOUTV-1030

--- web/src/frontend/js/components/oppfolgingsdialoger/Plan.js
+++ web/src/frontend/js/components/oppfolgingsdialoger/Plan.js
@@ -1,98 +1,50 @@
-import React, { Component, PropTypes } from 'react';
-import { getLedetekst } from 'digisyfo-npm';
+import React, { PropTypes } from 'react';
 import {
     OppfolgingsdialogSide,
-    OppfolgingsdialogSamtykke,
-    GodkjennPlanOversikt,
-    GodkjennPlanMottatt,
     GodkjennPlanSendt,
     GodkjentPlan,
 } from 'oppfolgingsdialog-npm';
+import GodkjennPlan from './GodkjennPlan';
 import { getContextRoot } from '../../routers/paths';
 
-export class Plan extends Component {
+export const Plan = ({ ledetekster, oppfolgingsdialog, oppfolgingsdialogId, giSamtykkeSvar }) => {
+    return (
+        <OppfolgingsdialogSide
+            brukernavn={oppfolgingsdialog.virksomhetsnavn}
+            ledetekster={ledetekster}
+            rootUrl={`/sykefravaer/oppfolgingsplaner/${oppfolgingsdialogId}`}>
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            side: 0,
-        };
-        this.forrigeSide = this.forrigeSide.bind(this);
-        this.nesteSide = this.nesteSide.bind(this);
-    }
+            {
+                !oppfolgingsdialog.godkjentAvArbeidstaker &&
+                <GodkjennPlan
+                    ledetekster={ledetekster}
+                    oppfolgingsdialog={oppfolgingsdialog}
+                    brukerType="SYKMELDT"
+                    giSamtykkeSvar={giSamtykkeSvar}
+                />
+            }
 
-    forrigeSide() {
-        this.setState({
-            side: this.state.side - 1,
-        });
-    }
-
-    nesteSide() {
-        this.setState({
-            side: this.state.side + 1,
-        });
-    }
-
-    render() {
-        const { ledetekster, oppfolgingsdialog, oppfolgingsdialogId, giSamtykkeSvar } = this.props;
-
-        return (
-            <OppfolgingsdialogSide
-                brukernavn={oppfolgingsdialog.virksomhetsnavn}
-                ledetekster={ledetekster}
-                rootUrl={`/sykefravaer/oppfolgingsplaner/${oppfolgingsdialogId}`}>
-                {
-                    !oppfolgingsdialog.godkjentAvArbeidstaker && !oppfolgingsdialog.godkjentAvArbeidsgiver && this.state.side === 0 &&
-                    <GodkjennPlanOversikt
-                        ledetekster={ledetekster}
-                        oppfolgingsdialog={oppfolgingsdialog}
-                        nesteSide={this.nesteSide}
-                        varselstripeTekst={getLedetekst('oppfolgingsdialog.arbeidstaker.godkjennplanskjema.varselstripe.tekst')}
-                        rootUrl={`${getContextRoot()}`}
-                    />
-                }
-                {
-                    !oppfolgingsdialog.godkjentAvArbeidstaker && oppfolgingsdialog.godkjentAvArbeidsgiver && this.state.side === 0 &&
-                    <GodkjennPlanMottatt
-                        ledetekster={ledetekster}
-                        oppfolgingsdialog={oppfolgingsdialog}
-                        nesteSide={this.nesteSide}
-                        brukerType="SYKMELDT"
-                        rootUrl={`${getContextRoot()}`}
-                    />
-                }
-                {
-                    (!oppfolgingsdialog.godkjentAvArbeidstaker || (!oppfolgingsdialog.godkjentAvArbeidstaker && oppfolgingsdialog.godkjentAvArbeidsgiver)) && this.state.side === 1 &&
-                        <OppfolgingsdialogSamtykke
-                            ledetekster={ledetekster}
-                            avbryt={this.forrigeSide}
-                            svgUrl="/sykefravaer/img/svg/samtykke.svg"
-                            svgAlt="samtykkeIllustrasjon"
-                            giSamtykkeSvar={giSamtykkeSvar}
-                        />
-                }
-                {
-                    oppfolgingsdialog.godkjentAvArbeidstaker && !oppfolgingsdialog.godkjentAvArbeidsgiver &&
-                    <GodkjennPlanSendt
-                        ledetekster={ledetekster}
-                        oppfolgingsdialog={oppfolgingsdialog}
-                        brukerType="SYKMELDT"
-                        rootUrl={`${getContextRoot()}`}
-                    />
-                }
-                {
-                    oppfolgingsdialog.godkjentAvArbeidstaker && oppfolgingsdialog.godkjentAvArbeidsgiver &&
-                    <GodkjentPlan
-                        ledetekster={ledetekster}
-                        oppfolgingsdialog={oppfolgingsdialog}
-                        brukerType="SYKMELDT"
-                        rootUrl={`${getContextRoot()}`}
-                    />
-                }
-            </OppfolgingsdialogSide>
-        );
-    }
-}
+            {
+                oppfolgingsdialog.godkjentAvArbeidstaker && !oppfolgingsdialog.godkjentAvArbeidsgiver &&
+                <GodkjennPlanSendt
+                    ledetekster={ledetekster}
+                    oppfolgingsdialog={oppfolgingsdialog}
+                    brukerType="SYKMELDT"
+                    rootUrl={`${getContextRoot()}`}
+                />
+            }
+            {
+                oppfolgingsdialog.godkjentAvArbeidstaker && oppfolgingsdialog.godkjentAvArbeidsgiver &&
+                <GodkjentPlan
+                    ledetekster={ledetekster}
+                    oppfolgingsdialog={oppfolgingsdialog}
+                    brukerType="SYKMELDT"
+                    rootUrl={`${getContextRoot()}`}
+                />
+            }
+        </OppfolgingsdialogSide>
+    );
+};
 
 Plan.propTypes = {
     ledetekster: PropTypes.object,

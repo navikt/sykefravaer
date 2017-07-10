464e9897fd87dff9963e12aca838290d938fd1ae
Erik Jansen <erik.gunnar.jansen@nav.no>
2017-07-10 14:54:16 +0200

    Legg til henting av pdfurler
    SYFOUTV-1030

--- web/src/frontend/js/sagas/index.js
+++ web/src/frontend/js/sagas/index.js
@@ -10,7 +10,14 @@ import vedlikeholdSagas from './vedlikeholdSagas';
 import pilotSykepengerSagas from './pilotSykepengerSagas';
 import moteSagas from './moteSagas';
 import svarSagas from './svarSagas';
-import { oppfolgingsdialogerAtSagas as oppfolgingsdialogerSagas, arbeidsoppgaveSagas, samtykkeSagas, tilgangSagas, tiltakSagas } from 'oppfolgingsdialog-npm';
+import {
+    oppfolgingsdialogerAtSagas as oppfolgingsdialogerSagas,
+    arbeidsoppgaveSagas,
+    dokumentSagas,
+    samtykkeSagas,
+    tilgangSagas,
+    tiltakSagas,
+} from 'oppfolgingsdialog-npm';
 import { ledeteksterSagas, togglesSagas } from 'digisyfo-npm';
 import forskutteringssporsmalSagas from './forskutteringssporsmalSagas';
 
@@ -32,6 +39,7 @@ export default function * rootSaga() {
         svarSagas(),
         oppfolgingsdialogerSagas(),
         arbeidsoppgaveSagas(),
+        dokumentSagas(),
         tilgangSagas(),
         tiltakSagas(),
         togglesSagas(),

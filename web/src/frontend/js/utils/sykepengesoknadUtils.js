c8da2c8e879de453cfd8220d2e5a161586382c20
Helge Fredheim <helge.fredheim@bekk.no>
2017-08-09 08:30:09 +0200

    Rettet feil oppstått under merge

--- web/src/frontend/js/utils/sykepengesoknadUtils.js
+++ web/src/frontend/js/utils/sykepengesoknadUtils.js
@@ -37,17 +37,6 @@ export const getSendtTilSuffix = (sykepengesoknad) => {
     return '';
 };
 
-
-export const sorterEtterOpprettetDato = (soknad1, soknad2) => {
-    if (soknad1.opprettetDato.getTime() > soknad2.opprettetDato.getTime()) {
-        return 1;
-    }
-    if (soknad1.opprettetDato.getTime() < soknad2.opprettetDato.getTime()) {
-        return -1;
-    }
-    return 0;
-};
-
 export const sorterEtterPerioder = (soknad1, soknad2) => {
     const soknad1Perioder = soknad1.aktiviteter.map((a) => {
         return a.periode;
@@ -93,39 +82,11 @@ export const mapAktiviteter = (soknad) => {
     return Object.assign({}, soknad, { aktiviteter });
 };
 
-
 export const sorterEtterOpprettetDato = (soknad1, soknad2) => {
     if (soknad1.opprettetDato.getTime() > soknad2.opprettetDato.getTime()) {
         return 1;
     }
     if (soknad1.opprettetDato.getTime() < soknad2.opprettetDato.getTime()) {
-        return -1;
-    }
-    return 0;
-};
-
-export const sorterEtterPerioder = (soknad1, soknad2) => {
-    const soknad1Perioder = soknad1.aktiviteter.map((a) => {
-        return a.periode;
-    });
-    const soknad2Perioder = soknad2.aktiviteter.map((a) => {
-        return a.periode;
-    });
-    const soknad1TidligsteFom = tidligsteFom(soknad1Perioder);
-    const soknad2TidligsteFom = tidligsteFom(soknad2Perioder);
-    const soknad1SenesteTom = senesteTom(soknad1Perioder);
-    const soknad2Senestetom = senesteTom(soknad2Perioder);
-
-    if (soknad1TidligsteFom.getTime() > soknad2TidligsteFom.getTime()) {
-        return -1;
-    }
-    if (soknad1TidligsteFom.getTime() < soknad2TidligsteFom.getTime()) {
-        return 1;
-    }
-    if (soknad1SenesteTom.getTime() > soknad2Senestetom.getTime()) {
-        return 1;
-    }
-    if (soknad1SenesteTom.getTime() < soknad2Senestetom.getTime()) {
         return -1;
     }
     return 0;

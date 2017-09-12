ec29249dc9c3f0b26c607ac01fe634c143ec65ae
Erik Jansen <erik.gunnar.jansen@nav.no>
2017-09-12 10:38:30 +0200

    Legg til border/ikon
    SYFOUTV-1372

--- web/src/frontend/js/components/oppfolgingsdialoger/UnderUtviklingVarsel.js
+++ web/src/frontend/js/components/oppfolgingsdialoger/UnderUtviklingVarsel.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { getLedetekst, Varselstripe } from 'digisyfo-npm';
+import { getContextRoot } from '../../routers/paths';
 
 const UnderUtviklingVarsel = () => {
     return (<div className="panel typo-infotekst blokk--l underUtvikling underUtvikling--erSynlig">
-        <Varselstripe>
+        <Varselstripe ikon={`${getContextRoot()}/img/svg/underUtviklingVarsel.svg`}>
             <p className="sist">{getLedetekst('under-utvikling.varsel.tekst')}</p>
         </Varselstripe>
     </div>);

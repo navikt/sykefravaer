79cf9b3bc490f682037290303446f52794b48dbe
Keyvan Honarvar <keyvan.honarvar@nav.no>
2017-10-25 15:29:55 +0200

    SYFOUTV-1534: Lagt til film.

--- web/src/frontend/js/components/oppfolgingsdialoger/Oppfolgingsdialoger.js
+++ web/src/frontend/js/components/oppfolgingsdialoger/Oppfolgingsdialoger.js
@@ -27,6 +27,7 @@ import {
 import UnderUtviklingVarsel from './UnderUtviklingVarsel';
 import IngenledereInfoboks from './IngenledereInfoboks';
 import { getContextRoot } from '../../routers/paths';
+import Video from '../Video';
 
 export const OppfolgingsdialogNyDialog = () => {
     return (
@@ -111,6 +112,13 @@ const Oppfolgingsdialoger = ({ oppfolgingsdialoger = [], ledetekster, avkreftLed
                     svgAlt="OppfølgingsdialogTidligere"
                 />
             </div>
+            }
+            {
+                <div className="panel">
+                    <h2 className="js-begrensning  typo-undertittel blokk--xxs">{getLedetekst('oppfolgingsdialog.filmsnutt.tittel')}</h2>
+                    <Video width="640" height="360" src="http://video.qbrick.com/play2/embed/player?accountId=763558&mediaId=25edfd35-00015227-b68553ad&configId=default&pageStyling=adaptive&&autoplay=false&repeat=false&sharing=false" />
+                    <p dangerouslySetInnerHTML={ {__html: getLedetekst('oppfolgingsdialog.filmsnutt.tekst', ledetekster) }} />
+                </div>
             }
         </div>);
     }

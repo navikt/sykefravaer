726d2950db5b5244fb2d831ec33f1e5436e8bf75
Erik Jansen <erik.gunnar.jansen@nav.no>
2017-10-26 10:15:11 +0200

    Oppfoelgingsdialog: Fjern lint feil

--- web/src/frontend/js/components/oppfolgingsdialoger/Oppfolgingsdialoger.js
+++ web/src/frontend/js/components/oppfolgingsdialoger/Oppfolgingsdialoger.js
@@ -117,7 +117,7 @@ const Oppfolgingsdialoger = ({ oppfolgingsdialoger = [], ledetekster, avkreftLed
                 <div className="panel">
                     <h2 className="js-begrensning lite-film typo-undertittel blokk--xxs">{getLedetekst('oppfolgingsdialog.filmsnutt.tittel')}</h2>
                     <Video width="476" height="360" src="https://video.qbrick.com/play2/embed/player?accountId=763558&mediaId=25edfd35-00015227-b68553ad&configId=default&pageStyling=adaptive&&autoplay=false&repeat=false&sharing=false" />
-                    <p dangerouslySetInnerHTML={ {__html: getLedetekst('oppfolgingsdialog.filmsnutt.tekst', ledetekster) }} />
+                    <p dangerouslySetInnerHTML={{ __html: getLedetekst('oppfolgingsdialog.filmsnutt.tekst', ledetekster) }} />
                 </div>
             }
         </div>);

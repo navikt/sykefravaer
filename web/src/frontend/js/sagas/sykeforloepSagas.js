3fc09df2a4bcf13a49127db4411a9fb908835c2b
Helge Fredheim <helge.fredheim@nav.no>
2018-01-03 08:52:53 +0100

    Lagrer mer statistikk om den sykmeldte i GA

--- web/src/frontend/js/sagas/sykeforloepSagas.js
+++ web/src/frontend/js/sagas/sykeforloepSagas.js
@@ -5,36 +5,11 @@ import * as actions from '../actions/sykeforloep_actions';
 import * as actiontyper from '../actions/actiontyper';
 import { get } from '../api';
 
-const getVarighet = (dato) => {
-    if (!dato) {
-        return 0;
-    }
-    const iDag = new Date();
-    const s = new Date(dato);
-    const ETT_DOGN = 1000 * 60 * 60 * 24;
-    const varighet = (iDag.getTime() - s.getTime()) / ETT_DOGN;
-    return Math.round(varighet);
-};
-
-const lagreSykmeldtStatistikk = (dato) => {
-    try {
-        const varighet = getVarighet(dato);
-        window.dataLayer.push({
-            'event': 'SYKEFORLOP_HENTET',
-            'varighet': varighet,
-            'dato': dato,
-        });
-    } catch (e) {
-        log(e);
-    }
-};
-
 export function* hentStartdato() {
     yield put(actions.henterStartdato());
     try {
         const respons = yield call(get, `${window.APP_SETTINGS.REST_ROOT}/tidslinje/startdato`);
         yield put(actions.startdatoHentet(respons.startdato));
-        lagreSykmeldtStatistikk(respons.startdato);
     } catch (e) {
         log(e);
         yield put(actions.hentStartdatoFeilet());

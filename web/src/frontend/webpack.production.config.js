a29bfc1b1589b37e9d6d4b88364d69208d00740c
Helge Fredheim <helge.fredheim@nav.no>
2018-05-09 14:09:48 +0200

    Retter lint-feil

--- web/src/frontend/webpack.production.config.js
+++ web/src/frontend/webpack.production.config.js
@@ -1,79 +1,79 @@
 var Webpack = require('webpack');
 var path = require('path');
-var nodeModulesPath = path.resolve(__dirname, 'node_modules');
-var buildPath = path.resolve(__dirname, "../main/webapp/js");
+var buildPath = path.resolve(__dirname, '../main/webapp/js');
 var mainPath = path.resolve(__dirname, 'js', 'index.js');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var autoprefixer = require("autoprefixer");
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var autoprefixer = require('autoprefixer');
 
 var config = function (opts) {
-  var timestamp = opts.timestamp;
-  var extractLess = new ExtractTextPlugin({
-    filename: "styles." + timestamp + ".css",
-    disable: false,
-  });
+    var timestamp = opts.timestamp;
+    var extractLess = new ExtractTextPlugin({
+        filename: 'styles.' + timestamp + '.css',
+        disable: false,
+    });
 
-  return {
+    return {
     // We change to normal source mapping
-    devtool: 'source-map',
-    entry: ['babel-polyfill', mainPath],
-    output: {
-      path: buildPath,
-      filename: 'bundle-prod.js'
-    },
-    resolve: {
-        alias: {
-            react: path.join(__dirname, 'node_modules', 'react')
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.less$/,
-                use: extractLess.extract({
-                  use: [{
-                    loader: "css-loader"
-                  }, {
-                    loader: "postcss-loader",
-                    options: {
-                        plugins: function() {
-                            return [autoprefixer]
-                        }
-                    }
-                  }, {
-                      loader: "less-loader", options: {
-                          paths: [
-                              path.resolve(__dirname, 'node_modules')
-                          ]
-                      }
-                  }]
-                })
-            },
-            {
-                test: /\.js$/,
-                exclude: [/node_modules/],
-                use: [{
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ["react", "env", "babel-preset-stage-0"]
-                    }
-                }]
-            },
-            {
-                test: /\.((woff2?|svg)(\?v=[0-9]\.[0-9]\.[0-9]))|(woff2?|svg|jpe?g|png|gif|ico)$/,
-                use: [{
-                    loader: 'svg-url-loader'
-                }]
+        devtool: 'source-map',
+        entry: ['babel-polyfill', mainPath],
+        output: {
+            path: buildPath,
+            filename: 'bundle-prod.js'
+        },
+        resolve: {
+            alias: {
+                react: path.join(__dirname, 'node_modules', 'react')
             }
-        ]
-    },
-    plugins: [
-      extractLess,
-      new Webpack.DefinePlugin({
-        "process.env.NODE_ENV": "'production'"
-      })
-    ]
-  };
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.less$/,
+                    use: extractLess.extract({
+                        use: [{
+                            loader: 'css-loader'
+                        }, {
+                            loader: 'postcss-loader',
+                            options: {
+                                plugins: function() {
+                                    return [autoprefixer]
+                                },
+                            },
+                        }, {
+                            loader: 'less-loader',
+                            options: {
+                                paths: [
+                                    path.resolve(__dirname, 'node_modules')
+                                ],
+                            },
+                        }],
+                    }),
+                },
+                {
+                    test: /\.js$/,
+                    exclude: [/node_modules/],
+                    use: [{
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['react', 'env', 'babel-preset-stage-0']
+                        },
+                    }],
+                },
+                {
+                    test: /\.((woff2?|svg)(\?v=[0-9]\.[0-9]\.[0-9]))|(woff2?|svg|jpe?g|png|gif|ico)$/,
+                    use: [{
+                        loader: 'svg-url-loader',
+                    }],
+                },
+            ],
+        },
+        plugins: [
+            extractLess,
+            new Webpack.DefinePlugin({
+                'process.env.NODE_ENV': '"production"'
+            }),
+        ],
+    };
 }
 
 module.exports = config;

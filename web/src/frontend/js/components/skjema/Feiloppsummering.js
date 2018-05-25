6ad7df4123e80f291e5bb0e144437c2418e5e4c2
Knut Nygaard <knut.nygaard@nav.no>
2018-05-25 14:19:02 +0200

    Logger om aiaiai visers og hvilke feil som er årsaken
    SYFOUTV-2106

--- web/src/frontend/js/components/skjema/Feiloppsummering.js
+++ web/src/frontend/js/components/skjema/Feiloppsummering.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { scrollTo, erSynligIViewport } from 'digisyfo-npm';
 import { Vis } from '../../utils';
+import logger from "../../logging";
 
 const FeillisteMelding = ({ feltnavn, feilmelding }) => {
     return (<li className="feiloppsummering__feil">
@@ -19,6 +20,7 @@ const getFeilmeldinger = (props) => {
 };
 
 class Feiloppsummering extends Component {
+
     componentDidUpdate(prevProps) {
         const { settFokus, skjemaErGyldig, skjemanavn } = this.props;
         if (settFokus) {

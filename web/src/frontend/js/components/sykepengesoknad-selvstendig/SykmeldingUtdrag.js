d1498ac9ec3520642538e3c8b31ce6f8252777b7
Helge Fredheim <helge.fredheim@nav.no>
2018-06-01 14:52:51 +0200

    Viser opplysninger om forsikring/fraværsperioder i sykmelding-utdrag

--- web/src/frontend/js/components/sykepengesoknad-selvstendig/SykmeldingUtdrag.js
+++ web/src/frontend/js/components/sykepengesoknad-selvstendig/SykmeldingUtdrag.js
@@ -1,6 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { SykmeldingPerioder, SykmeldingNokkelOpplysning, Utvidbar, toDatePrettyPrint, getLedetekst, sykmelding as sykmeldingPt } from 'digisyfo-npm';
+import { Vis } from '../../utils';
+
+const Fravaersperioder = ({ sykmelding }) => {
+    return (<SykmeldingNokkelOpplysning
+        tittel={getLedetekst('sykepengesoknad.sykmelding-utdrag.egenmelding-papir')}>
+        <ul>
+            {
+                sykmelding.sporsmal.fravaersperioder.map((p) => {
+                    return <li key={toDatePrettyPrint(p.fom)}>{toDatePrettyPrint(p.fom)} – {toDatePrettyPrint(p.tom)}</li>;
+                })
+            }
+        </ul>
+    </SykmeldingNokkelOpplysning>);
+};
+
+Fravaersperioder.propTypes = {
+    sykmelding: sykmeldingPt,
+};
+
+const Forsikring = ({ sykmelding }) => {
+    const grad = sykmelding.sporsmal.forsikring;
+    const nokkel = grad === null
+        ? 'sykepengesoknad.sykmelding-utdrag.forsikring-nei'
+        : 'sykepengesoknad.sykmelding-utdrag.forsikring-ja';
+    return (<SykmeldingNokkelOpplysning
+        tittel={getLedetekst('sykepengesoknad.sykmelding-utdrag.forsikring')}>
+        <p>
+            {
+                getLedetekst(nokkel, {
+                    '%GRAD%': grad,
+                })
+            }
+        </p>
+    </SykmeldingNokkelOpplysning>);
+};
+
+Forsikring.propTypes = {
+    sykmelding: sykmeldingPt,
+};
 
 const SykmeldingUtdrag = ({ erApen, sykmelding }) => {
     return (<div className="blokk">
@@ -23,6 +62,12 @@ const SykmeldingUtdrag = ({ erApen, sykmelding }) => {
                     tittel={getLedetekst('din-sykmelding.arbeidssituasjon.tittel.2')}>
                     <p className="js-arbeidssituasjon">{getLedetekst(`din-sykmelding.arbeidssituasjon.alternativ.${sykmelding.valgtArbeidssituasjon.toLowerCase()}`)}</p>
                 </SykmeldingNokkelOpplysning>
+                <Vis hvis={sykmelding.sporsmal.fravaerBesvart}>
+                    <Fravaersperioder sykmelding={sykmelding} />
+                </Vis>
+                <Vis hvis={sykmelding.sporsmal.forsikringBesvart}>
+                    <Forsikring sykmelding={sykmelding} />
+                </Vis>
             </div>
         </Utvidbar>
     </div>);
--- web/src/frontend/test/components/sykepengesoknad-selvstendig/SykmeldingUtdragTest.js
+++ web/src/frontend/test/components/sykepengesoknad-selvstendig/SykmeldingUtdragTest.js
@@ -1,5 +1,71 @@
 import chai from 'chai';
+import React from 'react';
+import { mount } from 'enzyme';
+import chaiEnzyme from 'chai-enzyme';
+import { setLedetekster, arbeidssituasjoner } from 'digisyfo-npm';
+import getSykmelding from '../../mockSykmeldinger';
+import SykmeldingUtdrag from '../../../js/components/sykepengesoknad-selvstendig/SykmeldingUtdrag';
+
+chai.use(chaiEnzyme());
+const expect = chai.expect;
 
 describe('SykmeldingUtdrag', () => {
-    // TODO: Skriv tester
+    let sykmelding;
+
+    beforeEach(() => {
+        setLedetekster({
+            'sykepengesoknad.sykmelding-utdrag.egenmelding-papir': 'Egenmelding og/eller sykmelding på papir',
+            'sykepengesoknad.sykmelding-utdrag.forsikring': 'Forsikring',
+            'sykepengesoknad.sykmelding-utdrag.forsikring-ja': 'Ja – %GRAD% % sykepenger fra dag 1',
+            'sykepengesoknad.sykmelding-utdrag.forsikring-nei': 'Nei',
+        });
+        sykmelding = getSykmelding({
+            valgtArbeidssituasjon: arbeidssituasjoner.NAERINGSDRIVENDE,
+            sporsmal: {
+                fravaersperioder: [{
+                    fom: new Date('2018-03-21'),
+                    tom: new Date('2018-03-24'),
+                }],
+                forsikring: 75,
+                arbeidssituasjon: arbeidssituasjoner.NAERINGSDRIVENDE,
+                fravaerBesvart: true,
+                forsikringBesvart: true,
+            },
+        });
+    });
+
+    it('Skal vise informasjon om fraværsperioder dersom dette spørsmålet er besvart', () => {
+        const component = mount(<SykmeldingUtdrag sykmelding={sykmelding} erApen />);
+        expect(component.text()).to.contain('Egenmelding og/eller sykmelding på papir');
+        expect(component.text()).to.contain('21.03.2018 – 24.03.2018');
+    });
+
+    it('Skal ikke vise informasjon om fraværsperioder dersom dette spørsmålet ikke er besvart', () => {
+        sykmelding.sporsmal.fravaerBesvart = false;
+        sykmelding.sporsmal.fravaersperioder = [];
+        const component = mount(<SykmeldingUtdrag sykmelding={sykmelding} erApen />);
+        expect(component.text()).not.to.contain('Egenmelding og/eller sykmelding på papir');
+        expect(component.text()).not.to.contain('21.03.2018 – 24.03.2018');
+    });
+
+    it('Skal vise informasjon om forsikring dersom dette spørsmålet er stilt', () => {
+        const component = mount(<SykmeldingUtdrag sykmelding={sykmelding} erApen />);
+        expect(component.text()).to.contain('Forsikring');
+        expect(component.text()).to.contain('Ja – 75 % sykepenger fra dag 1');
+    });
+
+    it('Skal vise informasjon om forsikring dersom dette spørsmålet er stilt og besvart med nei', () => {
+        sykmelding.sporsmal.forsikring = null;
+        const component = mount(<SykmeldingUtdrag sykmelding={sykmelding} erApen />);
+        expect(component.text()).to.contain('Forsikring');
+        expect(component.text()).to.contain('Nei');
+    });
+
+    it('Skal ikke vise informasjon om forsikring dersom dette spørsmålet ikke er stilt', () => {
+        sykmelding.sporsmal.forsikringBesvart = false;
+        sykmelding.sporsmal.forsikringBesvart = null;
+        const component = mount(<SykmeldingUtdrag sykmelding={sykmelding} erApen />);
+        expect(component.text()).not.to.contain('Forsikring');
+        expect(component.text()).not.to.contain('Ja – 75 % sykepenger fra dag 1');
+    });
 });

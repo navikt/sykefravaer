e1b0789136c5f23f25b8b0b8a49313e9416b6438
Helge Fredheim <helge.fredheim@nav.no>
2018-07-18 12:26:10 +0200

    Hotjar i sykmelding-kvittering for papirsøkende frilansere
    SYFOUTV-2302

--- web/src/frontend/js/components/sykmeldingkvittering/FrilanserMedPapirsoknadKvittering.js
+++ web/src/frontend/js/components/sykmeldingkvittering/FrilanserMedPapirsoknadKvittering.js
@@ -1,29 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { getLedetekst, Bjorn } from 'digisyfo-npm';
 import { getContextRoot } from '../../routers/paths';
 import Kvitteringsteg, { HtmlAvsnitt } from './Kvitteringsteg';
 
-export default () => {
-    return (<div className="js-kvittering js-kvittering--sok-papir">
-        <div className="panel blokk">
-            <div className="stegvisKvittering">
-                <Kvitteringsteg
-                    nummer="1"
-                    ok
-                    tittel={getLedetekst('bekreft-sykmelding.skal-opprettes-soknad.steg-1.tittel')} />
-                <Kvitteringsteg
-                    nummer="2"
-                    tittel={getLedetekst('bekreft-sykmelding.skal-opprettes-soknad.steg-2.tittel')}>
-                    <HtmlAvsnitt
-                        nokkel="bekreft-sykmelding.skal-opprettes-soknad.steg-2.tekst" />
-                </Kvitteringsteg>
+class FrilanserMedPapirsoknadKvittering extends Component {
+    componentDidMount() {
+        if (typeof window.hj === 'function') {
+            window.hj('trigger', 'SELVSTENDIG_FRILANS_JULI_2018');
+        }
+    }
+
+    render() {
+        return (<div className="js-kvittering js-kvittering--sok-papir">
+            <div className="panel blokk">
+                <div className="stegvisKvittering">
+                    <Kvitteringsteg
+                        nummer="1"
+                        ok
+                        tittel={getLedetekst('bekreft-sykmelding.skal-opprettes-soknad.steg-1.tittel')} />
+                    <Kvitteringsteg
+                        nummer="2"
+                        tittel={getLedetekst('bekreft-sykmelding.skal-opprettes-soknad.steg-2.tittel')}>
+                        <HtmlAvsnitt
+                            nokkel="bekreft-sykmelding.skal-opprettes-soknad.steg-2.tekst" />
+                    </Kvitteringsteg>
+                </div>
             </div>
-        </div>
-        <Bjorn
-            className="blokk"
-            hvit
-            stor
-            nokkel="bekreft-sykmelding.skal-opprettes-soknad.bjorn"
-            rootUrl={getContextRoot()} />
-    </div>);
-};
+            <Bjorn
+                className="blokk"
+                hvit
+                stor
+                nokkel="bekreft-sykmelding.skal-opprettes-soknad.bjorn"
+                rootUrl={getContextRoot()} />
+        </div>);
+    }
+}
+
+export default FrilanserMedPapirsoknadKvittering;

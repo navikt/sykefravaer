15105e26af18f9241174e5ed129bac739e604bb0
Helge Fredheim <helge.fredheim@nav.no>
2018-07-20 12:36:46 +0200

    Ny toggle som gjør det mulig å deaktivere hele applikasjonen

--- web/src/frontend/js/toggles.js
+++ web/src/frontend/js/toggles.js
@@ -23,3 +23,9 @@ export const toggleBrukMockdataUtland = () => {
     const url = window.location.href;
     return url.indexOf('localhost') > -1;
 };
+
+export const toggleHeleAppen = () => {
+    // Hvis denne settes til false, deaktiveres hele appen og det vises en plakat i stedet
+    // Tekst hardkodes i Side.js
+    return true;
+}

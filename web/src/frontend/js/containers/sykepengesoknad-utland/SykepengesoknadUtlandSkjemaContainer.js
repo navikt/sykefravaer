1d44786cdfca1d9b534ceab2e9797b7b152314ae
Trude Jostad <trude.jostad@nav.no>
2018-07-23 11:52:07 +0200

    Legger til kvittering for opphold utland søknad

--- web/src/frontend/js/containers/sykepengesoknad-utland/SykepengesoknadUtlandSkjemaContainer.js
+++ web/src/frontend/js/containers/sykepengesoknad-utland/SykepengesoknadUtlandSkjemaContainer.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { getLedetekst } from 'digisyfo-npm';
 import { soknad as soknadPt } from '../../propTypes';
 import { NY } from '../../enums/soknadstatuser';
 import UtlandsSkjema from '../../components/sykepengesoknad-utland/UtlandsSkjema/UtlandsSkjema';
 import { sendSoknad } from '../../actions/soknader_actions';
 import PropTypes from 'prop-types';
-
+import Kvittering from '../../components/sykepengesoknad-utland/Kvittering/Kvittering';
 
 
 export const SykepengesoknadUtlandSkjemaContainer = ({ soknad, sendSoknad, sender }) => {
-            if (soknad) {
-                return (<UtlandsSkjema
-                    soknad = {soknad}
-                    sendSoknad = {sendSoknad}
-                    sender = {sender}
-                />);
-            }
-            return (<p className="panel begrensning">
-                    Lat som om vi redirecter til fremsiden!
-    </p>); // redirect til fremside
+    if (soknad) {
+        return (<UtlandsSkjema
+            soknad={soknad}
+            sendSoknad={sendSoknad}
+            sender={sender}
+        />);
+    }
+    return (<Kvittering />);
 };
 
 SykepengesoknadUtlandSkjemaContainer.propTypes = {

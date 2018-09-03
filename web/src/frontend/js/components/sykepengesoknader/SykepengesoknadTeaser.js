0932034838325a4d4d6d02e68e93ae3c8ac7d313
Helge Fredheim <helge.fredheim@nav.no>
2018-09-03 09:13:10 +0200

    Refaktorerer SykepengesoknadTeaser

--- web/src/frontend/js/components/sykepengesoknader/SykepengesoknadTeaser.js
+++ web/src/frontend/js/components/sykepengesoknader/SykepengesoknadTeaser.js
@@ -42,9 +42,104 @@ const visIkonHover = (soknadstype) => {
         : <img alt="" className="js-ikon" src="/sykefravaer/img/svg/soknader_hover-blue.svg" />;
 };
 
-const SykepengesoknadTeaser = ({ soknad }) => {
-    const visStatus = [NY, SENDT, AVBRUTT].indexOf(soknad.status) === -1;
+const beregnUndertekst = (soknad) => {
     const sendtTilBeggeMenIkkeSamtidig = erSendtTilBeggeMenIkkeSamtidig(soknad);
+    if (soknad.status === AVBRUTT) {
+        return getLedetekst('soknad.teaser.status.AVBRUTT', {
+            '%DATO%': tilLesbarDatoMedArstall(soknad.avbruttDato),
+        });
+    }
+    if (soknad.status !== SENDT && soknad.status !== TIL_SENDING && !soknad.soknadstype) {
+        return getLedetekst('soknad.teaser.undertekst', { '%ARBEIDSGIVER%': soknad.arbeidsgiver.navn });
+    }
+    if (sendtTilBeggeMenIkkeSamtidig && soknad.status !== NY) {
+        return <SendtUlikt soknad={soknad} />;
+    }
+    if (soknad.status !== NY && soknad.status !== UTKAST_TIL_KORRIGERING) {
+        if ((soknad.soknadstype === OPPHOLD_UTLAND || soknad.soknadstype === SELVSTENDIGE_OG_FRILANSERE) && soknad.status === SENDT) {
+            return getLedetekst('soknad.teaser.status.SENDT.til-nav', {
+                '%DATO%': tilLesbarDatoMedArstall(soknad.innsendtDato),
+            });
+        }
+        return getLedetekst(`soknad.teaser.status.${soknad.status}${getSendtTilSuffix(soknad)}`, {
+            '%DATO%': tilLesbarDatoMedArstall(soknad.sendtTilArbeidsgiverDato || soknad.sendtTilNAVDato),
+            '%ARBEIDSGIVER%': soknad.arbeidsgiver ? soknad.arbeidsgiver.navn : null,
+        });
+    }
+    return null;
+};
+
+export const TeaserUndertekst = ({ soknad }) => {
+    const tekst = beregnUndertekst(soknad);
+    return tekst ? (<p className="inngangspanel__undertekst js-undertekst mute">
+        {tekst}
+    </p>) : null;
+};
+
+TeaserUndertekst.propTypes = {
+    soknad: PropTypes.oneOfType([sykepengesoknadPt, soknadPt]).isRequired,
+};
+
+export const TeaserStatus = ({ soknad }) => {
+    const visStatus = [NY, SENDT, AVBRUTT].indexOf(soknad.status) === -1;
+    return visStatus ? (<p className="inngangspanel__status js-status">
+        {
+            getLedetekst(`soknad.teaser.status.${soknad.status}`, {
+                '%DATO%': tilLesbarDatoMedArstall(soknad.sendtTilArbeidsgiverDato || soknad.sendtTilNAVDato),
+            })
+        }
+    </p>) : null;
+};
+
+TeaserStatus.propTypes = {
+    soknad: PropTypes.oneOfType([sykepengesoknadPt, soknadPt]).isRequired,
+};
+
+export const TeaserTittel = ({ soknad }) => {
+    return (<h3 className="js-title" id={`soknad-header-${soknad.id}`}>
+        <small className="inngangspanel__meta js-meta">
+            {
+                getLedetekst('soknad.teaser.dato', {
+                    '%DATO%':
+                        tilLesbarDatoMedArstall(
+                            soknad.soknadstype === OPPHOLD_UTLAND || soknad.soknadstype === SELVSTENDIGE_OG_FRILANSERE
+                                ? soknad.opprettetDato
+                                : soknad.tom,
+                        ),
+                })
+            }
+        </small>
+        <span className="inngangspanel__tittel">
+            {
+                getLedetekst(soknad.soknadstype === OPPHOLD_UTLAND
+                    ? 'soknad.utland.teaser.tittel'
+                    : 'soknad.teaser.tittel')
+            }
+        </span>
+    </h3>);
+};
+
+TeaserTittel.propTypes = {
+    soknad: PropTypes.oneOfType([sykepengesoknadPt, soknadPt]).isRequired,
+};
+
+export const TeaserPeriode = ({ soknad }) => {
+    return soknad.soknadstype !== OPPHOLD_UTLAND
+        ? (<p className="inngangspanel__tekst js-tekst">
+            {
+                getLedetekst('soknad.teaser.tekst', {
+                    '%PERIODE%': tilLesbarPeriodeMedArstall(soknad.fom, soknad.tom),
+                })
+            }
+        </p>)
+        : null;
+};
+
+TeaserPeriode.propTypes = {
+    soknad: PropTypes.oneOfType([sykepengesoknadPt, soknadPt]).isRequired,
+};
+
+const SykepengesoknadTeaser = ({ soknad }) => {
     const status = soknad.status ? soknad.status.toLowerCase() : '';
     return (<article aria-labelledby={`soknader-header-${soknad.id}`}>
         <Link
@@ -58,81 +153,11 @@ const SykepengesoknadTeaser = ({ soknad }) => {
             </span>
             <div className="inngangspanel__innhold">
                 <header className="inngangspanel__header">
-                    <h3 className="js-title" id={`soknad-header-${soknad.id}`}>
-                        <small className="inngangspanel__meta js-meta">
-                            {
-                                (() => {
-                                    return getLedetekst('soknad.teaser.dato', {
-                                        '%DATO%':
-                                            tilLesbarDatoMedArstall(soknad.soknadstype === OPPHOLD_UTLAND || soknad.soknadstype === SELVSTENDIGE_OG_FRILANSERE
-                                                ? soknad.opprettetDato : soknad.tom),
-                                    });
-                                })()
-                            }
-                        </small>
-                        <span className="inngangspanel__tittel">
-                            {
-                                (() => {
-                                    if (soknad.soknadstype === OPPHOLD_UTLAND) {
-                                        return getLedetekst('soknad.utland.teaser.tittel');
-                                    }
-                                    return getLedetekst('soknad.teaser.tittel');
-                                })()
-                            }
-                        </span>
-                    </h3>
-                    {
-                        visStatus &&
-                        <p className="inngangspanel__status js-status">
-                            {
-                                getLedetekst(`soknad.teaser.status.${soknad.status}`, {
-                                    '%DATO%': tilLesbarDatoMedArstall(soknad.sendtTilArbeidsgiverDato || soknad.sendtTilNAVDato),
-                                })
-                            }
-                        </p>
-                    }
+                    <TeaserTittel soknad={soknad} />
+                    <TeaserStatus soknad={soknad} />
                 </header>
-                <p className="inngangspanel__tekst js-tekst">
-                    {
-                        (() => {
-                            if (soknad.soknadstype !== OPPHOLD_UTLAND) {
-                                return getLedetekst('soknad.teaser.tekst', {
-                                    '%PERIODE%': tilLesbarPeriodeMedArstall(soknad.fom, soknad.tom),
-                                });
-                            }
-                            return null;
-                        })()
-                    }
-                </p>
-                <p className="inngangspanel__undertekst js-undertekst mute">
-                    {
-                        (() => {
-                            if (soknad.status === AVBRUTT) {
-                                return getLedetekst('soknad.teaser.status.AVBRUTT', {
-                                    '%DATO%': tilLesbarDatoMedArstall(soknad.avbruttDato),
-                                });
-                            }
-                            if (soknad.status !== SENDT && soknad.status !== TIL_SENDING && !soknad.soknadstype) {
-                                return getLedetekst('soknad.teaser.undertekst', { '%ARBEIDSGIVER%': soknad.arbeidsgiver.navn });
-                            }
-                            if (sendtTilBeggeMenIkkeSamtidig && soknad.status !== NY) {
-                                return <SendtUlikt soknad={soknad} />;
-                            }
-                            if (soknad.status !== NY && soknad.status !== UTKAST_TIL_KORRIGERING) {
-                                if ((soknad.soknadstype === OPPHOLD_UTLAND || soknad.soknadstype === SELVSTENDIGE_OG_FRILANSERE) && soknad.status === SENDT) {
-                                    return getLedetekst('soknad.teaser.status.SENDT.til-nav', {
-                                        '%DATO%': tilLesbarDatoMedArstall(soknad.innsendtDato),
-                                    });
-                                }
-                                return getLedetekst(`soknad.teaser.status.${soknad.status}${getSendtTilSuffix(soknad)}`, {
-                                    '%DATO%': tilLesbarDatoMedArstall(soknad.sendtTilArbeidsgiverDato || soknad.sendtTilNAVDato),
-                                    '%ARBEIDSGIVER%': soknad.arbeidsgiver ? soknad.arbeidsgiver.navn : null,
-                                });
-                            }
-                            return null;
-                        })()
-                    }
-                </p>
+                <TeaserPeriode soknad={soknad} />
+                <TeaserUndertekst soknad={soknad} />
             </div>
         </Link>
     </article>);

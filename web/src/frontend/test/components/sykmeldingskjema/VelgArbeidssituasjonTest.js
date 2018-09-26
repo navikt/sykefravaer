c041428f08a3ce21b6c6a14bd866c7d5ae7438af
Helge Fredheim <helge.fredheim@nav.no>
2018-09-26 12:08:25 +0200

    Endrer valg av arbeidssituasjon i sykmelding
    SYFOUTV-2462

--- web/src/frontend/test/components/sykmeldingskjema/VelgArbeidssituasjonTest.js
+++ web/src/frontend/test/components/sykmeldingskjema/VelgArbeidssituasjonTest.js
@@ -3,27 +3,58 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { Field } from 'redux-form';
 import chaiEnzyme from 'chai-enzyme';
-import VelgArbeidssituasjon, { RendreVelgArbeidssituasjon, Velg } from '../../../js/components/sykmeldingskjema/VelgArbeidssituasjon';
-import VelgArbeidsgiverContainer from '../../../js/containers/sykmelding/VelgArbeidsgiverContainer';
+import { arbeidssituasjoner, setLedetekster } from 'digisyfo-npm';
+import VelgArbeidssituasjon, { getAlternativer, RendreVelgArbeidssituasjon, Velg, visTillegg } from '../../../js/components/sykmeldingskjema/VelgArbeidssituasjon';
 import SporsmalMedTillegg from '../../../js/components/skjema/SporsmalMedTillegg';
 import Radioknapper from '../../../js/components/skjema/Radioknapper';
+import SkrivUtSykmeldingDialog from '../../../js/components/sykmeldingskjema/SkrivUtSykmeldingDialog';
+import ErLederRiktig from '../../../js/components/sykmeldingskjema/ErLederRiktig';
 
 chai.use(chaiEnzyme());
 const expect = chai.expect;
 
 describe('VelgArbeidssituasjon', () => {
+    let props;
+    const arbeidsgivereMock = [
+        {
+            navn: 'TESTBEDRIFT',
+            orgnummer: '111222333',
+            naermesteLeder: {
+                aktoerId: '123456789',
+                navn: 'Testleder Testledernavn',
+                epost: 'test@test.no',
+                mobil: '11223344',
+                orgnummer: '111222333',
+                organisasjonsnavn: null,
+                aktivTom: null,
+                arbeidsgiverForskuttererLoenn: true,
+            },
+            stilling: 'TESTJOBB',
+        },
+    ];
+
+    beforeEach(() => {
+        setLedetekster({
+            'din-sykmelding.arbeidssituasjon.alternativ.arbeidstaker.2': 'jobb hos en arbeidsgiver',
+            'din-sykmelding.arbeidssituasjon.alternativ.arbeidstaker-annen-arbeidsgiver.2': 'jobb hos en annen arbeidsgiver',
+            'din-sykmelding.arbeidssituasjon.alternativ.naeringsdrivende.2': 'jobb som selvstendig næringsdrivende',
+        });
+        props = {
+            arbeidsgivere: [],
+            input: {},
+        };
+    });
+
     it('Har et field med Velg', () => {
-        const comp = shallow(<VelgArbeidssituasjon />);
+        const comp = shallow(<VelgArbeidssituasjon {...props} />);
         const field = comp.find(Field);
         expect(field.props().component).to.deep.equal(Velg);
     });
 
     describe('Velg', () => {
         let component;
-        let props;
 
         beforeEach(() => {
-            props = {};
             component = shallow(<Velg {...props} />);
         });
 
@@ -32,32 +63,160 @@ describe('VelgArbeidssituasjon', () => {
             expect(component.find(SporsmalMedTillegg).prop('Sporsmal')).to.deep.equal(<RendreVelgArbeidssituasjon {...props} />);
         });
 
-        it('Skal inneholde SporsmalMedTillegg med riktig children', () => {
-            expect(component.find(SporsmalMedTillegg).contains(<VelgArbeidsgiverContainer {...props} />)).to.equal(true);
+        it('Skal vise SkrivUtSykmeldingDialog hvis arbeidssituasjon er ARBEIDSTAKER', () => {
+            props.input.value = arbeidssituasjoner.ARBEIDSTAKER;
+            component = shallow(<Velg {...props} />);
+            expect(component.find(SporsmalMedTillegg).find(SkrivUtSykmeldingDialog)).to.have.length(1);
         });
 
-        it("Skal vise tillegg hvis arbeidssituasjon === 'ARBEIDSTAKER'", () => {
+        it('Skal vise spørsmål om nærmeste leder hvis det er valgt en arbeidsgiver', () => {
+            props.input = {
+                value: '111222333',
+            };
+            props.arbeidsgivere = arbeidsgivereMock;
+            component = shallow(<Velg {...props} />);
+            expect(component.find(SporsmalMedTillegg).find(ErLederRiktig)).to.have.length(1);
+        });
+
+        it('Skal vise tillegg hvis arbeidssituasjon er ARBEIDSTAKER', () => {
             props.input = {
                 value: 'ARBEIDSTAKER',
             };
+            component = shallow(<Velg {...props} />);
             expect(component.find(SporsmalMedTillegg).prop('visTillegg')(props)).to.equal(true);
         });
 
-        it("Skal ikke vise tillegg hvis arbeidssituasjon === 'noe annet enn ARBEIDSTAKER'", () => {
+
+        it('Skal ikke vise tillegg hvis arbeidssituasjon === "noe annet enn ARBEIDSTAKER"', () => {
             props.input = {
                 value: 'noe annet enn ARBEIDSTAKER',
             };
+            component = shallow(<Velg {...props} />);
             expect(component.find(SporsmalMedTillegg).prop('visTillegg')(props)).to.equal(false);
         });
     });
 
-    describe('RendreVelgArbeidssituasjon', () => {
-        it('Har viser 5 radioknapper', () => {
-            const input = { value: null };
-            const meta = {};
-            const comp = shallow(<RendreVelgArbeidssituasjon input={input} meta={meta} />);
+    describe('RendreVelgArbeidssituasjon når bruker har arbeidsgivere', () => {
+        it('Viser i utgangspunktet 2 radioknapper', () => {
+            props.alternativer = getAlternativer(arbeidsgivereMock);
+            const comp = shallow(<RendreVelgArbeidssituasjon {...props} />);
             expect(comp.find(Radioknapper)).to.have.length(1);
-            expect(comp.find('i').length).to.be.equal(5);
+            expect(comp.find('i').length).to.equal(2);
+        });
+
+        it('Viser alle alternativer dersom state.visArbeidssituasjoner === true', () => {
+            props.alternativer = getAlternativer(arbeidsgivereMock, { visArbeidssituasjoner: true });
+            const comp = shallow(<RendreVelgArbeidssituasjon {...props} />);
+            expect(comp.find(Radioknapper)).to.have.length(1);
+            expect(comp.find('i').length).to.equal(6);
+        });
+    });
+
+    describe('RendreVelgArbeidssituasjon når bruker har arbeidsgivere', () => {
+        it('Viser i utgangspunktet 2 radioknapper', () => {
+            props.alternativer = getAlternativer(arbeidsgivereMock);
+            const comp = shallow(<RendreVelgArbeidssituasjon {...props} />);
+            expect(comp.find(Radioknapper)).to.have.length(1);
+            expect(comp.find('i').length).to.equal(2);
+        });
+
+        it('Viser alle alternativer dersom state.visArbeidssituasjoner === true', () => {
+            props.alternativer = getAlternativer(arbeidsgivereMock, {
+                visArbeidssituasjoner: true,
+            });
+            const comp = shallow(<RendreVelgArbeidssituasjon {...props} />);
+            expect(comp.find(Radioknapper)).to.have.length(1);
+        });
+
+        it('Viser alternativene i riktig rekkefølge', () => {
+            props.alternativer = getAlternativer(arbeidsgivereMock, {
+                visArbeidssituasjoner: true,
+            });
+            const comp = shallow(<RendreVelgArbeidssituasjon {...props} />);
+            expect(comp.find('i').at(0).prop('value')).to.equal('111222333');
+            expect(comp.find('i').at(1).prop('value')).to.equal(arbeidssituasjoner.NAERINGSDRIVENDE);
+            expect(comp.find('i').at(1).prop('label')).to.equal('jobb som selvstendig næringsdrivende');
+            expect(comp.find('i').at(2).prop('value')).to.equal(arbeidssituasjoner.FRILANSER);
+            expect(comp.find('i').at(3).prop('value')).to.equal(arbeidssituasjoner.ARBEIDSTAKER);
+            expect(comp.find('i').at(3).prop('label')).to.equal('jobb hos en annen arbeidsgiver');
+            expect(comp.find('i').at(4).prop('value')).to.equal(arbeidssituasjoner.ARBEIDSLEDIG);
+            expect(comp.find('i').at(5).prop('value')).to.equal(arbeidssituasjoner.ANNET);
+        });
+    });
+
+    describe('RendreVelgArbeidssituasjon når bruker IKKE har arbeidsgivere', () => {
+        it('Viser i utgangspunktet alle radioknapper', () => {
+            props.alternativer = getAlternativer([]);
+            const comp = shallow(<RendreVelgArbeidssituasjon {...props} />);
+            expect(comp.find(Radioknapper)).to.have.length(1);
+            expect(comp.find('i').length).to.equal(5);
+        });
+
+        it('Viser alternativene i riktig rekkefølge', () => {
+            props.alternativer = getAlternativer([], { visArbeidssituasjoner: true });
+            const comp = shallow(<RendreVelgArbeidssituasjon {...props} />);
+            expect(comp.find('i').at(0).prop('value')).to.equal(arbeidssituasjoner.NAERINGSDRIVENDE);
+            expect(comp.find('i').at(1).prop('value')).to.equal(arbeidssituasjoner.FRILANSER);
+            expect(comp.find('i').at(2).prop('value')).to.equal(arbeidssituasjoner.ARBEIDSTAKER);
+            expect(comp.find('i').at(2).prop('label')).to.equal('jobb hos en arbeidsgiver');
+            expect(comp.find('i').at(3).prop('value')).to.equal(arbeidssituasjoner.ARBEIDSLEDIG);
+            expect(comp.find('i').at(4).prop('value')).to.equal(arbeidssituasjoner.ANNET);
+        });
+    });
+
+    describe("visTillegg", () => {
+        it("Skal returnere false når bruker har strengt fortrolig adresse", () => {
+            const vis = visTillegg({
+                harStrengtFortroligAdresse: true,
+                input: {},
+                arbeidsgivere: [],
+            });
+            expect(vis).to.equal(false);
+        });
+
+        it("Skal returnere true når bruker har valgt en arbeidsgiver med nærmeste leder", () => {
+            const vis = visTillegg({
+                harStrengtFortroligAdresse: false,
+                input: {
+                    value: '111222333',
+                },
+                arbeidsgivere: arbeidsgivereMock,
+            });
+            expect(vis).to.equal(true);
+        });
+
+        it("Skal returnere false når bruker har valgt en arbeidsgiver uten nærmeste leder", () => {
+            const vis = visTillegg({
+                harStrengtFortroligAdresse: false,
+                input: {
+                    value: '000111222',
+                },
+                arbeidsgivere: [
+                    ...arbeidsgivereMock,
+                    {
+                        orgnummer: '000111222',
+                        naermesteLeder: null,
+                    },
+                ],
+            });
+            expect(vis).to.equal(false);
+        });
+
+        it("Skal returnere true når bruker har valgt arbeidssituasjon 'ARBEIDSTAKER'", () => {
+            const vis = visTillegg({
+                harStrengtFortroligAdresse: false,
+                input: {
+                    value: 'ARBEIDSTAKER',
+                },
+                arbeidsgivere: [
+                    ...arbeidsgivereMock,
+                    {
+                        orgnummer: '000111222',
+                        naermesteLeder: null,
+                    },
+                ],
+            });
+            expect(vis).to.equal(true);
         });
     });
 });

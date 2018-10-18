61bda6769a560afa79421db48006f06adc67d916
Knut Nygaard <knutnyg@gmail.com>
2018-10-18 09:59:49 +0200

    Tilpassninger etter flat filstruktur + litt opprydning

--- deploy-heroku.sh
+++ deploy-heroku.sh
@@ -1,24 +1,9 @@
 #!/bin/bash
 
-cd web/src/frontend
-
 npm install
 npm run lint
 npm run test
 npm run nais-build
-
-cp -r dist ../../../.
-cp .env ../../../.
-cp server.js ../../../.
-cp decorator.js ../../../.
-cp settings.json ../../../.
-cp package.json ../../../.
-
-cp mockEndepunkter.js ../../../.
-mkdir -p ../../../test/mock
-cp -r ./test/mock/* ../../../test/mock/
-
-cd ../../../
 
 # Build docker image
 docker build -t sykefravaer -f Dockerfile.web --build-arg BASE_IMAGE_PREFIX=node . 

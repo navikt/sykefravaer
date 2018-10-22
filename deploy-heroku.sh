be35eec679c69a6c5b0829031f6d5c281a81de57
Martin Solheim <martin.solheim@me.com>
2018-10-22 13:10:55 +0200

    Oppdaterer readme og fikser deployscript

--- deploy-heroku.sh
+++ deploy-heroku.sh
@@ -5,8 +5,11 @@ npm run lint
 npm run test
 npm run nais-build
 
+mkdir -p mock
+cp ./test/mock/* ./mock/
+
 # Build docker image
-docker build -t sykefravaer -f Dockerfile.web --build-arg BASE_IMAGE_PREFIX=node . 
+docker build -t sykefravaer -f Dockerfile.web --build-arg BASE_IMAGE_PREFIX=node .
 
 # Push docker image
 heroku container:push web -a sykefravaer --recursive

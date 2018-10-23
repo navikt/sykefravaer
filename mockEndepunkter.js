c02cbd0e5ba0a256ab8eac83f4d7594640b332ea
Helge Fredheim <helge.fredheim@nav.no>
2018-10-23 12:22:23 +0200

    Logger vellykket henting av tekster fra prod

--- mockEndepunkter.js
+++ mockEndepunkter.js
@@ -68,21 +68,26 @@ lastFilTilMinne(VIRKSOMHET);
 
 let teksterFraProd;
 
-function hentTeksterFraProd() {
+function hentTeksterFraProd(loggSuksess) {
     const TEKSTER_URL = 'https://syfoapi.nav.no/syfotekster/api/tekster';
     request(TEKSTER_URL, function (error, response, body) {
         if (error) {
             console.log('Kunne ikke hente tekster fra prod', error);
         } else {
-            teksterFraProd = body;
+            teksterFraProd = JSON.parse(body);
+            if (loggSuksess) {
+                console.log('Tekster hentet fra prod');
+            }
         }
     });
 }
 
 function mockTekster(server) {
     const HVERT_FEMTE_MINUTT = 1000 * 60 * 5;
-    hentTeksterFraProd();
-    setInterval(hentTeksterFraProd, HVERT_FEMTE_MINUTT);
+    hentTeksterFraProd(true);
+    setInterval(() => {
+        hentTeksterFraProd();
+    }, HVERT_FEMTE_MINUTT);
 
     server.get('/syfotekster/api/tekster', (req, res) => {
         res.setHeader('Content-Type', 'application/json');
@@ -346,6 +351,14 @@ function mockForOpplaeringsmiljo(server) {
     server.get('/syforest/informasjon/toggles', (req, res) => {
         res.setHeader('Content-Type', 'application/json');
         res.send(JSON.stringify(mockData[TOGGLES]));
+    });
+
+    server.get('/esso/logout', (req, res) => {
+        res.send('<p>Du har blitt sendt til utlogging.</p><p><a href="/sykefravaer">Gå til Ditt sykefravær</a></p>');
+    });
+
+    server.get('/dittnav', (req, res) => {
+        res.send('<p>Ditt Nav er ikke tilgjengelig - dette er en testside som kun viser Ditt sykefravær.</p><p><a href="/sykefravaer">Gå til Ditt sykefravær</a></p>');
     });
 }
 

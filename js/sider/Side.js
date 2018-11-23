8a9c210407e85abce860d0bf1204e018ee80a88e
Erik Jansen <erik.gunnar.jansen@nav.no>
2018-11-23 16:30:52 +0100

    Prodfix: Fjern fullbredde pga feil og at ft ikke er paa

--- js/sider/Side.js
+++ js/sider/Side.js
@@ -78,10 +78,10 @@ export class SideComponent extends Component {
     }
 
     render() {
-        const { children, tittel, brodsmuler = [], laster, begrenset, erInnlogget, fullBredde } = this.props;
+        const { children, tittel, brodsmuler = [], laster, begrenset, erInnlogget } = this.props;
         const sideClassNames = getClassNames(laster, erInnlogget);
         const innholdClassNames = cn('side__innhold', {
-            'side__innhold--begrenset js-begrensning': !fullBredde && (begrenset || !erInnlogget || !toggleHeleAppen()),
+            'side__innhold--begrenset js-begrensning': (begrenset || !erInnlogget || !toggleHeleAppen()),
         });
         setAppClass(laster, erInnlogget);
         return (<DocumentTitle title={tittel + (tittel.length > 0 ? ' - www.nav.no' : 'www.nav.no')}>
@@ -118,7 +118,6 @@ SideComponent.propTypes = {
     laster: PropTypes.bool,
     begrenset: PropTypes.bool,
     erInnlogget: PropTypes.bool,
-    fullBredde: PropTypes.bool,
     sjekkInnlogging: PropTypes.func,
 };
 

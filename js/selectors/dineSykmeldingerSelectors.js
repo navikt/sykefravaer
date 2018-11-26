164b49f9bcaa103d0671cf34228d3f9de9357ec2
Helge Fredheim <helge.fredheim@nav.no>
2018-11-26 12:41:34 +0100

    Sykmelding-kvittering takler refresh av siden

--- js/selectors/dineSykmeldingerSelectors.js
+++ js/selectors/dineSykmeldingerSelectors.js
@@ -1,16 +1,22 @@
 import { sykmeldingHarBehandletSoknad } from './soknaderSelectors';
 import { toggleSykmeldingEndreArbeidssituasjon } from './unleashTogglesSelectors';
 
-export const skalHenteDineSykmeldinger = (state) => {
+export const skalHenteDineSykmeldingerSelector = (state) => {
     return !state.dineSykmeldinger.henter
         && !state.dineSykmeldinger.hentet
         && !state.dineSykmeldinger.hentingFeilet;
 };
 
-export const kanEndreSykmeldingArbeidssituasjon = (state, sykmelding) => {
+export const kanEndreSykmeldingArbeidssituasjonSelector = (state, sykmelding) => {
     const FIRE_MANEDER_SIDEN = new Date();
     FIRE_MANEDER_SIDEN.setMonth(FIRE_MANEDER_SIDEN.getMonth() - 4);
     return sykmelding.sendtdato > FIRE_MANEDER_SIDEN
         && !sykmeldingHarBehandletSoknad(state, sykmelding.id)
         && toggleSykmeldingEndreArbeidssituasjon(state);
+};
+
+export const finnDinSykmeldingSelector = (state, sykmeldingId) => {
+    return state.dineSykmeldinger.data.find((s) => {
+        return s.id === sykmeldingId;
+    });
 };

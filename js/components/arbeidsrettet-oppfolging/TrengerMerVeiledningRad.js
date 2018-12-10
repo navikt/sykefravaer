d60b5e90f0ddfa51f7482d2dbf5d5f481793dee1
Alexander Gård <alexander.gard@nav.no>
2018-12-10 09:34:53 +0100

    FO-1803 Justering på URL til arbeidssokerregistrering

--- js/components/arbeidsrettet-oppfolging/TrengerMerVeiledningRad.js
+++ js/components/arbeidsrettet-oppfolging/TrengerMerVeiledningRad.js
@@ -11,7 +11,7 @@ const handleNeiBtnClicked = () => {
 
 const handleJaBtnClicked = () => {
     // TODO: Legg til API-kall for å registrere knappetrykk
-    window.location.href = '/arbeidssokerregistrering?fraSykefravaer=true';
+    window.location.href = '/arbeidssokerregistrering/start?fraSykefravaer=true';
 };
 
 const TrengerMerVeiledningRad = () => {

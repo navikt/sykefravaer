80e5a1fcdf55160210ca6ff14bbce09300243033
Martin Solheim <martin.solheim@me.com>
2019-01-16 09:56:48 +0100

    Skriver om til component og bruker callback for redirect
    Redirect feiler nå bekreftkallet feiler

--- js/components/arbeidsrettet-oppfolging/TrengerMerVeiledningRad.js
+++ js/components/arbeidsrettet-oppfolging/TrengerMerVeiledningRad.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as PT from 'prop-types';
 import { Normaltekst, Undertittel } from 'nav-frontend-typografi';
@@ -7,46 +7,62 @@ import { getLedetekst } from 'digisyfo-npm';
 import history from '../../history';
 import { bekreftMerVeiledning } from '../../actions/merVeiledning_actions';
 import { selectAlleHarMerVeiledningIder } from '../../reducers/hendelser';
+import { hentHendelser } from '../../actions/hendelser_actions';
 
-const TrengerMerVeiledningRad = ({ doBekreftMerVeiledning, merVeiledningHendelseIder }) => {
-    const bekreftAlleMerVeiledninghendelser = (callback) => {
-        merVeiledningHendelseIder.forEach((id) => {
-            doBekreftMerVeiledning(id)
-                .then(callback);
+class TrengerMerVeiledningRad extends Component {
+    constructor(props) {
+        super(props);
+
+        this.handleNeiBtnClicked = this.handleNeiBtnClicked.bind(this);
+        this.handleJaBtnClicked = this.handleJaBtnClicked.bind(this);
+        this.bekreftAlleMerVeiledninghendelser = this.bekreftAlleMerVeiledninghendelser.bind(this);
+    }
+
+    componentDidMount() {
+        this.props.doHentHendelser();
+    }
+
+    bekreftAlleMerVeiledninghendelser(callback) {
+        this.props.merVeiledningHendelseIder.forEach((id) => {
+            this.props.doBekreftMerVeiledning(id, callback);
         });
-    };
+    }
 
-    const handleNeiBtnClicked = () => {
-        bekreftAlleMerVeiledninghendelser();
-        history.push(`${process.env.REACT_APP_CONTEXT_ROOT}`);
-    };
-
-    const handleJaBtnClicked = () => {
-        bekreftAlleMerVeiledninghendelser(() => {
-            window.location.href = '/arbeidssokerregistrering/?fraSykefravaer=true';
+    handleNeiBtnClicked() {
+        this.bekreftAlleMerVeiledninghendelser(() => {
+            return history.push('/sykefravaer');
         });
-    };
+    }
 
-    return (
-        <div className="infoside-fo__rad infoside-fo__rad--graa">
-            <div className="begrensning">
-                <Undertittel className="blokk-s">{getLedetekst('infoside-fo.veiledning.overskrift')}</Undertittel>
-                <Normaltekst className="blokk-xs">{getLedetekst('infoside-fo.veiledning.tekst')}</Normaltekst>
-                <div className="infoside-fo__knapperad">
-                    <Knapp onClick={handleNeiBtnClicked}>
-                        {getLedetekst('infoside-fo.knapp-nei')}
-                    </Knapp>
-                    <Hovedknapp onClick={handleJaBtnClicked}>
-                        {getLedetekst('infoside-fo.knapp-ja')}
-                    </Hovedknapp>
+    handleJaBtnClicked() {
+        this.bekreftAlleMerVeiledninghendelser(() => {
+            return window.location.assign('/arbeidssokerregistrering/?fraSykefravaer=true');
+        });
+    }
+
+    render() {
+        return (
+            <div className="infoside-fo__rad infoside-fo__rad--graa">
+                <div className="begrensning">
+                    <Undertittel className="blokk-s">{getLedetekst('infoside-fo.veiledning.overskrift')}</Undertittel>
+                    <Normaltekst className="blokk-xs">{getLedetekst('infoside-fo.veiledning.tekst')}</Normaltekst>
+                    <div className="infoside-fo__knapperad">
+                        <Knapp onClick={this.handleNeiBtnClicked}>
+                            {getLedetekst('infoside-fo.knapp-nei')}
+                        </Knapp>
+                        <Hovedknapp onClick={this.handleJaBtnClicked}>
+                            {getLedetekst('infoside-fo.knapp-ja')}
+                        </Hovedknapp>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
 TrengerMerVeiledningRad.propTypes = {
     doBekreftMerVeiledning: PT.func,
+    doHentHendelser: PT.func,
     merVeiledningHendelseIder: PT.arrayOf(PT.number),
 };
 
@@ -58,6 +74,7 @@ const mapStateToProps = (state) => {
 
 const actionCreators = {
     doBekreftMerVeiledning: bekreftMerVeiledning,
+    doHentHendelser: hentHendelser,
 };
 
 export default connect(

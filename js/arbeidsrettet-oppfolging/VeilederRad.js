ffaa5a014496fdcd2036713ae7116a783c31f419
Helge Fredheim <helge.fredheim@bekk.no>
2019-03-05 15:42:54 +0100

    Gir FO-siden en liten overhaling. Fikser luft, tar i bruk Bjørn og fjerner 'Hei <fornavn>'.

--- js/arbeidsrettet-oppfolging/VeilederRad.js
+++ js/arbeidsrettet-oppfolging/VeilederRad.js
@@ -1,38 +1,20 @@
-import Veilederpanel from 'nav-frontend-veilederpanel';
-import { Systemtittel } from 'nav-frontend-typografi';
-import { getHtmlLedetekst, getLedetekst } from '@navikt/digisyfo-npm';
+import { getHtmlLedetekst, Bjorn } from '@navikt/digisyfo-npm';
 import React from 'react';
 import PropTypes from 'prop-types';
-import { hentFornavn } from '../utils/index';
 
-const VeilederRad = ({ brukerNavn, maksDato }) => {
-    const veilederpanelKompakt = window.matchMedia('(min-width: 768px)').matches;
-    const veilederpanelType = veilederpanelKompakt ? 'normal' : 'plakat';
-    const fornavn = hentFornavn(brukerNavn);
-
+const VeilederRad = ({ maksDato }) => {
     return (
         <div className="infoside-fo__rad infoside-fo__rad--graa">
             <div className="begrensning">
-                <Veilederpanel
-                    svg={<img src={`${process.env.REACT_APP_CONTEXT_ROOT}/img/svg/infoside-fo/veileder-mann.svg`} alt="Veileder" className="nav-veilederpanel__illustrasjon" />}
-                    type={veilederpanelType}
-                    kompakt={veilederpanelKompakt}
-                >
-                    <div>
-                        <Systemtittel className="blokk-xs">{getLedetekst('infoside-fo.intro.overskrift', { '%NAVN%': fornavn })}</Systemtittel>
-                        <div
-                            className="typo-normal"
-                            dangerouslySetInnerHTML={getHtmlLedetekst('infoside-fo.intro.tekst', { '%DATO%': maksDato })}
-                        />
-                    </div>
-                </Veilederpanel>
+                <Bjorn stor hvit>
+                    <div dangerouslySetInnerHTML={getHtmlLedetekst('infoside-fo.intro.tekst', { '%DATO%': maksDato })} />
+                </Bjorn>
             </div>
         </div>
     );
 };
 
 VeilederRad.propTypes = {
-    brukerNavn: PropTypes.string,
     maksDato: PropTypes.string,
 };
 

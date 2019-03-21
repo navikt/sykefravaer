11501fc3e4f824b3b3dba77c45b991b9529e78bd
Helge Fredheim <helge.fredheim@bekk.no>
2019-03-21 09:20:03 +0100

    Valideringsfeil start

--- js/sykepengesoknad/soknad-arbeidstaker/aktiviteter-i-sykmeldingsperioden/validerAktiviteterISykmeldingsperioden.js
+++ js/sykepengesoknad/soknad-arbeidstaker/aktiviteter-i-sykmeldingsperioden/validerAktiviteterISykmeldingsperioden.js
@@ -3,11 +3,13 @@ import validerSporsmal from '../../validering/validerSporsmal';
 import validerFravaerOgFriskmelding from '../fravar-og-friskmelding/validerFravaerOgFriskmelding';
 
 export default (values, props) => {
+    console.log(JSON.stringify(props));
     const sporsmal = hentSporsmalForAktiviteterISykmeldingsperioden(props.soknad);
     const feilmeldinger = validerSporsmal(sporsmal, values);
     const feilmeldingerForrigeSide = validerFravaerOgFriskmelding(values, props);
-    return {
+    const alleFeilmeldinger = {
         ...feilmeldinger,
         ...feilmeldingerForrigeSide,
     };
+    return alleFeilmeldinger;
 };

0b904f90c56576fbd3f2136d92dede30a5acf0b1
Helge Fredheim <helge.fredheim@bekk.no>
2019-04-03 18:00:33 +0200

    Viser oppsummering på siste side

--- js/sykepengesoknad/soknad-arbeidstaker/ett-sporsmal-per-side/EttSporsmalPerSide.js
+++ js/sykepengesoknad/soknad-arbeidstaker/ett-sporsmal-per-side/EttSporsmalPerSide.js
@@ -3,17 +3,24 @@ import PropTypes from 'prop-types';
 import { sykmelding as sykmeldingPt } from '@navikt/digisyfo-npm';
 import history from '../../../history';
 import Soknadskjema from '../../felleskomponenter/Soknadskjema';
-import { soknadPt } from '../../../propTypes';
+import { skjemasvar as skjemasvarPt, soknadMetaReducerPt, soknadPt } from '../../../propTypes';
 import Sporsmalsliste from '../../felleskomponenter/sporsmal/Sporsmalsliste';
 import AvbrytSoknadContainer from '../../felleskomponenter/avbryt-soknad/AvbrytSoknadContainer';
 import FeiloppsummeringContainer from '../../../containers/skjema/FeiloppsummeringContainer';
 import { getSkjemanavnFraSoknad } from '../../utils/getSkjemanavnFraSoknad';
 import AppSpinner from '../../../components/AppSpinner';
 import KnapperadEttSporsmalPerSide from './KnapperadEttSporsmalPerSide';
+import { VAER_KLAR_OVER_AT } from '../../enums/tagtyper';
+import { SykepengesoknadArbeidstakerOppsummeringSkjema } from '../oppsummering/Oppsummering';
 
 export const hentSporsmalForDenneSiden = (soknad, sidenummer) => {
     const sporsmal = soknad.sporsmal[sidenummer - 1];
     return [sporsmal];
+};
+
+const erSisteSide = (soknad, sidenummer) => {
+    const sporsmal = hentSporsmalForDenneSiden(soknad, sidenummer);
+    return sporsmal[0].tag === VAER_KLAR_OVER_AT;
 };
 
 const GenereltEttSporsmalPerSideSkjema = (props) => {
@@ -42,7 +49,8 @@ GenereltEttSporsmalPerSideSkjema.propTypes = {
 };
 
 const EttSporsmalPerSide = (props) => {
-    const { sykmelding, soknad, handleSubmit, actions, sidenummer, oppdaterer } = props;
+    const { sykmelding, soknad, handleSubmit, actions, sidenummer, oppdaterer, skjemasvar, sendingFeilet, soknadMeta, sender } = props;
+    const Komponent = erSisteSide(soknad, sidenummer) ? SykepengesoknadArbeidstakerOppsummeringSkjema : GenereltEttSporsmalPerSideSkjema;
     return (<Soknadskjema
         apentUtdrag={sidenummer === 1}
         sykmelding={sykmelding}
@@ -50,9 +58,13 @@ const EttSporsmalPerSide = (props) => {
         {
             oppdaterer
                 ? <AppSpinner />
-                : <GenereltEttSporsmalPerSideSkjema
+                : <Komponent
                     soknad={soknad}
+                    sykmelding={sykmelding}
                     handleSubmit={handleSubmit}
+                    skjemasvar={skjemasvar}
+                    sendingFeilet={sendingFeilet || soknadMeta.hentingFeilet}
+                    sender={sender}
                     actions={actions}
                     sidenummer={sidenummer} />
         }
@@ -65,9 +77,14 @@ EttSporsmalPerSide.propTypes = {
     handleSubmit: PropTypes.func,
     actions: PropTypes.shape({
         lagreSoknad: PropTypes.func,
+        sendSoknad: PropTypes.func,
     }),
     sidenummer: PropTypes.number,
     oppdaterer: PropTypes.bool,
+    skjemasvar: skjemasvarPt,
+    sendingFeilet: PropTypes.bool,
+    sender: PropTypes.bool,
+    soknadMeta: soknadMetaReducerPt,
 };
 
 export default EttSporsmalPerSide;

c4b068231641b4e2b743893edc887eb221993399
Helge Fredheim <helge.fredheim@bekk.no>
2019-04-12 09:40:36 +0200

    Metrikker på arbeidsretttet oppfølging. Logger til GTM.

--- js/arbeidsrettet-oppfolging/DinOkonomi.js
+++ js/arbeidsrettet-oppfolging/DinOkonomi.js
@@ -1,9 +1,13 @@
 import { getHtmlLedetekst, getLedetekst } from '@navikt/digisyfo-npm';
 import React from 'react';
 import ArbeidsrettetOppfolgingUtvidbar from './ArbeidsrettetOppfolgingUtvidbar';
+import { pushToDataAOLayer } from './pushToAODataLayer';
 
 const DinOkonomi = () => {
     return (<ArbeidsrettetOppfolgingUtvidbar
+        onClick={() => {
+            pushToDataAOLayer('LES_MER_AAP');
+        }}
         tittel={getLedetekst('ao.okonomi.tittel')}
         introTekst={getHtmlLedetekst('ao.okonomi.tekst')}
         utvidbarTittel={getLedetekst('ao.aap.tittel')}

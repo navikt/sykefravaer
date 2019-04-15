644406b55ec22bbe2d368900231032f0b9571f3c
Helge Fredheim <helge.fredheim@bekk.no>
2019-04-15 21:40:24 +0200

    Retter feil med erOppdelt

--- js/components/soknad-felles/SykepengersoknadBanner.js
+++ js/components/soknad-felles/SykepengersoknadBanner.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { getLedetekst, tilLesbarPeriodeMedArstall } from '@navikt/digisyfo-npm';
+import { connect } from 'react-redux';
+import { getLedetekst, tilLesbarPeriodeMedArstall, sykmelding as sykmeldingPt } from '@navikt/digisyfo-npm';
 import PropTypes from 'prop-types';
 import Hjelpetekst from 'nav-frontend-hjelpetekst';
 import * as proptypes from '../../propTypes';
@@ -8,32 +9,35 @@ import { OPPHOLD_UTLAND } from '../../sykepengesoknad/enums/soknadtyper';
 import { settErOppdelt as settErOppdeltGammel } from '../../sykepengesoknad-gammel-plattform/data/sykepengesoknader/sykepengesoknader';
 import { settErOppdelt as settErOppdeltNy } from '../../sykepengesoknad/utils/settErOppdelt';
 
-const SykepengesoknadBanner = ({ soknad }) => {
-    const settErOppdelt = soknad && soknad.soknadstype ? settErOppdeltNy : settErOppdeltGammel;
-
-    const { _erOppdelt } = settErOppdelt(soknad);
+const SykepengesoknadBanner = ({ soknad, sykmelding, henter }) => {
+    const settErOppdelt = soknad && soknad.soknadstype
+        ? settErOppdeltNy
+        : settErOppdeltGammel;
+    const { _erOppdelt } = settErOppdelt(soknad, sykmelding);
     const tittel = soknad && soknad.soknadstype === OPPHOLD_UTLAND
         ? getLedetekst('sykepengesoknad-utland.tittel')
         : getLedetekst('sykepengesoknad.sidetittel');
 
-    return (<React.Fragment>
-        <header className="soknadtopp">
-            <h1 className="soknadtopp__tittel">{tittel}</h1>
-            {
-                _erOppdelt
-                && (<div className="medHjelpetekst sidetopp__meta begrensning">
-                    <p>
-                        {
-                            getLedetekst('sykepengesoknad.sidetittel.periode-2', {
-                                '%PERIODE%': tilLesbarPeriodeMedArstall(soknad.fom, soknad.tom),
-                            })
-                        }
-                    </p>
-                    <Hjelpetekst id="oppdelt-soknad-hjelpetekst">{getLedetekst('sykepengesoknad.sidetittel.hjelpetekst.tekst')}</Hjelpetekst>
-                </div>)
-            }
-        </header>
-    </React.Fragment>);
+    return henter
+        ? null
+        : (<React.Fragment>
+            <header className="soknadtopp">
+                <h1 className="soknadtopp__tittel">{tittel}</h1>
+                {
+                    _erOppdelt
+                    && (<div className="medHjelpetekst sidetopp__meta begrensning">
+                        <p>
+                            {
+                                getLedetekst('sykepengesoknad.sidetittel.periode-2', {
+                                    '%PERIODE%': tilLesbarPeriodeMedArstall(soknad.fom, soknad.tom),
+                                })
+                            }
+                        </p>
+                        <Hjelpetekst id="oppdelt-soknad-hjelpetekst">{getLedetekst('sykepengesoknad.sidetittel.hjelpetekst.tekst')}</Hjelpetekst>
+                    </div>)
+                }
+            </header>
+        </React.Fragment>);
 };
 
 SykepengesoknadBanner.propTypes = {
@@ -41,6 +45,22 @@ SykepengesoknadBanner.propTypes = {
         proptypes.sykepengesoknad,
         soknadPt,
     ]),
+    sykmelding: sykmeldingPt,
+    henter: PropTypes.bool,
 };
 
-export default SykepengesoknadBanner;
+const mapStateToProps = (state, ownProps) => {
+    return {
+        henter: state.dineSykmeldinger.henter
+            || state.ledetekster.henter
+            || state.soknader.henter
+            || state.sykepengesoknader.henter,
+        sykmelding: ownProps.soknad
+            ? state.dineSykmeldinger.data.find((s) => {
+                return s.id === ownProps.soknad.sykmeldingId;
+            })
+            : undefined,
+    };
+};
+
+export default connect(mapStateToProps)(SykepengesoknadBanner);

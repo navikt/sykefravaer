54d32e00ebb5be1aad063dbe9df7bfe636e311f3
Martin Solheim <martin.solheim@me.com>
2019-05-21 14:28:12 +0200

    Bruker ledetekster

--- js/sykmeldinger/sykmelding-avvist/SykmeldingOpplysninger.js
+++ js/sykmeldinger/sykmelding-avvist/SykmeldingOpplysninger.js
@@ -1,6 +1,8 @@
 /* eslint arrow-body-style: ["error", "as-needed"] */
 import React from 'react';
 import cn from 'classnames';
+import PT from 'prop-types';
+import { getLedetekst } from '@navikt/digisyfo-npm';
 import { getDuration, tilLesbarPeriodeMedArstall } from '@navikt/digisyfo-npm/lib/utils/datoUtils';
 import { sorterPerioderEldsteFoerst } from '@navikt/digisyfo-npm/lib/utils/sorterSykmeldingerUtils';
 import { smSykmeldingPeriodePt, smSykmeldingPerioderPt, smSykmeldingPt } from '../../propTypes/smSykmeldingProptypes';
@@ -8,18 +10,19 @@ import { smSykmeldingPeriodePt, smSykmeldingPerioderPt, smSykmeldingPt } from '.
 export const SykmeldingOpplysninger = ({ smSykmelding }) => (
     <article>
         <div className="panel blokk dine-opplysninger">
-            <h2 className="typo-innholdstittel blokk-l">Sykmelding</h2>
+            <h2 className="typo-innholdstittel blokk-l">{getLedetekst('din-sykmelding.tittel')}</h2>
 
             <div className="blokk-l side-innhold">
                 <SykmeldingPerioder perioder={smSykmelding.sykmeldingsperioder} />
-                <div className="nokkelopplysning">
-                    <h2 className="nokkelopplysning__tittel">Arbeidsgiver som legen har skrevet inn</h2>
-                    <p className="js-arbeidsgiver">{smSykmelding.arbeidsgiverNavn}</p>
-                </div>
-                <div className="nokkelopplysning">
-                    <h2 className="nokkelopplysning__tittel">Lege / sykmelder</h2>
-                    <p className="js-avsender">{smSykmelding.legeNavn}</p>
-                </div>
+                <Nokkelopplysning
+                    vis={smSykmelding.arbeidsgiver !== null}
+                    tittel={getLedetekst('din-sykmelding.arbeidsgiver.tittel')}
+                    tekst={smSykmelding.arbeidsgiver ? smSykmelding.arbeidsgiver.navn : ''}
+                />
+                <Nokkelopplysning
+                    tittel={getLedetekst('din-sykmelding.avsender.tittel')}
+                    tekst={smSykmelding.legeNavn}
+                />
             </div>
         </div>
     </article>
@@ -27,6 +30,24 @@ export const SykmeldingOpplysninger = ({ smSykmelding }) => (
 
 SykmeldingOpplysninger.propTypes = {
     smSykmelding: smSykmeldingPt,
+};
+
+
+const Nokkelopplysning = ({ vis = true, tittel, tekst }) => (
+    vis
+        ? (
+            <div className="nokkelopplysning">
+                <h2 className="nokkelopplysning__tittel">{tittel}</h2>
+                <p className="js-avsender">{tekst}</p>
+            </div>
+        )
+        : null
+);
+
+Nokkelopplysning.propTypes = {
+    vis: PT.bool,
+    tittel: PT.string.isRequired,
+    tekst: PT.string.isRequired,
 };
 
 const perioderClass = perioder => cn('sykmeldingPerioder', { 'sykmeldingPerioder--flere': perioder.length > 1 });
@@ -46,15 +67,17 @@ SykmeldingPerioder.propTypes = {
 
 const SykmeldingPeriode = ({ periode }) => {
     const antallDager = getDuration(periode.fom, periode.tom);
-    const grad = periode.type === 'AKTIVITET_IKKE_MULIG' ? 100 : periode.grad ? periode.grad : '';
+    const dagerBoying = antallDager > 1 ? getLedetekst('din-sykmelding.periode.dager') : getLedetekst('din-sykmelding.periode.dag');
+    const grad = periode.type === 'AKTIVITET_IKKE_MULIG' ? 100 : periode.gradert.grad ? periode.gradert.grad : '';
+
     return (
         <div className="nokkelopplysning">
-            <h2 className="nokkelopplysning__tittel">Periode</h2>
+            <h2 className="nokkelopplysning__tittel">{getLedetekst('din-sykmelding.periode.tittel')}</h2>
             <p className="js-periode blokk-xxs">
-                <strong>{tilLesbarPeriodeMedArstall(periode.fom, periode.tom)}</strong> &bull; {antallDager}&nbsp;{antallDager > 1 ? 'dager' : 'dag'}
+                <strong>{tilLesbarPeriodeMedArstall(periode.fom, periode.tom)}</strong> &bull; {antallDager}&nbsp;{dagerBoying}
             </p>
             <p className="js-grad">
-                {grad} % sykmeldt
+                {getLedetekst('din-sykmelding.grad.tekst', { '%GRAD%': grad })}
             </p>
         </div>
     );

8ffbc069db2c00814c3ba32525a211c844817e8f
Martin Solheim <martin.solheim@me.com>
2019-05-23 16:53:16 +0200

    Viser avvistdato i statuspanel

--- js/sykmeldinger/sykmelding-avvist/AvvistSykmeldingStatuspanel.js
+++ js/sykmeldinger/sykmelding-avvist/AvvistSykmeldingStatuspanel.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import Statuspanel, { StatusNokkelopplysning, Statusopplysninger } from '../../components/Statuspanel';
+import { StatusNokkelopplysning } from '../../components/Statuspanel';
 import { tilLesbarDatoMedArstall } from '../../utils/datoUtils';
 import { smSykmeldingPt } from '../../propTypes/smSykmeldingProptypes';
 
 export const AvvistSykmeldingStatuspanel = ({ smSykmelding }) => {
     return smSykmelding.bekreftetDato ?
-        (<Statuspanel className="blokk--xl">
-            <Statusopplysninger>
+        (
+            <div className="panel panel--komprimert statuspanel blokk--xl statuspanel--avvistSykmelding">
                 <StatusNokkelopplysning tittel="Status">
                     <p>Avvist av NAV</p>
+                </StatusNokkelopplysning>
+                <StatusNokkelopplysning tittel="Dato avvist">
+                    <p>{tilLesbarDatoMedArstall(smSykmelding.mottattTidspunkt)}</p>
                 </StatusNokkelopplysning>
                 <StatusNokkelopplysning tittel="Bekreftet av deg">
                     <p>{tilLesbarDatoMedArstall(smSykmelding.bekreftetDato)}</p>
                 </StatusNokkelopplysning>
-            </Statusopplysninger>
-        </Statuspanel>)
+            </div>
+        )
         : null;
 };
 

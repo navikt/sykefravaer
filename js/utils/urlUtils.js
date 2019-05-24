f6542faa2a22d69b02e8cf99850cae6da98ef439
Helge Fredheim <helge.fredheim@bekk.no>
2019-05-24 08:53:22 +0200

    Riktig URL til sykepengesøknad når appen kjøres på Heroku

--- js/utils/urlUtils.js
+++ js/utils/urlUtils.js
@@ -4,7 +4,9 @@ export const erPaaHeroku = () => {
 };
 
 export const getSykepengesoknaderUrl = () => {
-    return process.env.REACT_APP_SYKEPENGESOKNAD_ROOT;
+    return erPaaHeroku()
+        ? 'https://sykepengesoknad.herokuapp.com/sykepengesoknad'
+        : process.env.REACT_APP_SYKEPENGESOKNAD_ROOT;
 };
 
 export const getSykepengesoknadUrl = (soknadId) => {

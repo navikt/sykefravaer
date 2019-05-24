26f3cff4153102fb60bfbca70eb46ccaa212cf60
Martin Solheim <martin.solheim@me.com>
2019-05-24 11:44:18 +0200

    Bruker statuspanel styling med egne regler for tre kollonner

--- js/sykmeldinger/sykmelding-avvist/AvvistSykmeldingStatuspanel.js
+++ js/sykmeldinger/sykmelding-avvist/AvvistSykmeldingStatuspanel.js
@@ -1,25 +1,28 @@
+/* eslint arrow-body-style: ["error", "as-needed"] */
 import React from 'react';
-import { StatusNokkelopplysning } from '../../components/Statuspanel';
+import { StatusNokkelopplysning, Statusopplysninger } from '../../components/Statuspanel';
 import { tilLesbarDatoMedArstall } from '../../utils/datoUtils';
 import { smSykmeldingPt } from '../../propTypes/smSykmeldingProptypes';
 
-export const AvvistSykmeldingStatuspanel = ({ smSykmelding }) => {
-    return smSykmelding.bekreftetDato ?
-        (
-            <div className="panel panel--komprimert statuspanel blokk--xl statuspanel--avvistSykmelding">
-                <StatusNokkelopplysning tittel="Status">
-                    <p>Avvist av NAV</p>
-                </StatusNokkelopplysning>
-                <StatusNokkelopplysning tittel="Dato avvist">
-                    <p>{tilLesbarDatoMedArstall(smSykmelding.mottattTidspunkt)}</p>
-                </StatusNokkelopplysning>
-                <StatusNokkelopplysning tittel="Bekreftet av deg">
-                    <p>{tilLesbarDatoMedArstall(smSykmelding.bekreftetDato)}</p>
-                </StatusNokkelopplysning>
+export const AvvistSykmeldingStatuspanel = ({ smSykmelding }) => (
+    smSykmelding.bekreftetDato
+        ? (
+            <div className="panel panel--komprimert statuspanel blokk--xl statuspanel--treKol">
+                <Statusopplysninger>
+                    <StatusNokkelopplysning tittel="Status">
+                        <p>Avvist av NAV</p>
+                    </StatusNokkelopplysning>
+                    <StatusNokkelopplysning tittel="Dato avvist">
+                        <p>{tilLesbarDatoMedArstall(smSykmelding.mottattTidspunkt)}</p>
+                    </StatusNokkelopplysning>
+                    <StatusNokkelopplysning tittel="Bekreftet av deg">
+                        <p>{tilLesbarDatoMedArstall(smSykmelding.bekreftetDato)}</p>
+                    </StatusNokkelopplysning>
+                </Statusopplysninger>
             </div>
         )
-        : null;
-};
+        : null
+);
 
 AvvistSykmeldingStatuspanel.propTypes = {
     smSykmelding: smSykmeldingPt,

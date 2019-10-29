bdc73105825bfb3892a0217f625c086706852a04
Martin Solheim <martin.solheim@me.com>
2019-10-29 16:12:04 +0100

    Tar i bruk nginx proxy innenfor toggle
    Bruker toggle slik at vi raskt kan bytte mellom ny og gammel proxy.

--- js/sykmeldinger/data/sm-sykmeldinger/smSykmeldingerSagas.js
+++ js/sykmeldinger/data/sm-sykmeldinger/smSykmeldingerSagas.js
@@ -1,33 +1,53 @@
-import {
-    call, put, fork, takeEvery, all, select, delay,
-} from 'redux-saga/effects';
+import { all, call, delay, fork, put, select, takeEvery } from 'redux-saga/effects';
 import { log } from '@navikt/digisyfo-npm';
 import { browserHistory } from 'react-router';
 import {
     BEKREFT_LEST_SM_SYKMELDING_FORESPURT,
-    HENT_SM_SYKMELDINGER_FORESPURT,
     bekrefterLestSmSykmelding,
+    bekreftSmSykmeldingKvitteringVistLengeNok,
+    HENT_SM_SYKMELDINGER_FORESPURT,
     henterSmSykmeldinger,
     hentSmSykmeldingerFeilet,
+    smSykmeldingBekreftetLest,
     smSykmeldingBekreftLestFeilet,
     smSykmeldingerHentet,
-    smSykmeldingBekreftetLest,
-    bekreftSmSykmeldingKvitteringVistLengeNok,
 } from './smSykmeldingerActions';
 import { skalBekrefteSmSykmeldingSelector, skalHenteSmSykmeldingerSelector } from './smSykmeldingerSelectors';
-import {
-    API_NAVN, get, hentSyfoApiUrl, post,
-} from '../../../data/gateway-api';
-import { toggleNyttSykmeldingsmottak, unleashtogglesHentetSelector } from '../../../data/unleash-toggles/unleashTogglesSelectors';
+import { API_NAVN, get, hentSyfoApiUrl, post } from '../../../data/gateway-api';
+import { toggleBrukNginxProxy, toggleNyttSykmeldingsmottak, unleashtogglesHentetSelector } from '../../../data/unleash-toggles/unleashTogglesSelectors';
 import { HENTET_UNLEASH_TOGGLES } from '../../../data/unleash-toggles/unleashToggles_actions';
+
+export const hentSykmeldingsregisterUrl = (brukNginxProxy) => {
+    const url = window
+    && window.location
+    && window.location.href
+        ? window.location.href
+        : '';
+
+    if (!brukNginxProxy) {
+        return hentSyfoApiUrl(API_NAVN.SYFOSMREGISTER);
+    }
+
+    if (url.indexOf('tjenester.nav') > -1) {
+        // Prod
+        return 'https://syfosmregisterproxy.nav.no';
+    }
+    if (url.indexOf('localhost') > -1 || url.indexOf('herokuapp') > -1) {
+        // Lokalt
+        return '/syfosmregister';
+    }
+    // Preprod
+    return 'https://syfosmregisterproxy-q.nav.no';
+};
 
 export function* oppdaterSmSykmeldinger() {
     const toggle = yield select(toggleNyttSykmeldingsmottak);
     const toggleHentet = yield select(unleashtogglesHentetSelector);
+    const brukNginxProxyToggle = yield select(toggleBrukNginxProxy);
     if (toggle && toggleHentet) {
         yield put(henterSmSykmeldinger());
         try {
-            const data = yield call(get, `${hentSyfoApiUrl(API_NAVN.SYFOSMREGISTER)}/v1/sykmeldinger`);
+            const data = yield call(get, `${hentSykmeldingsregisterUrl(brukNginxProxyToggle)}/v1/sykmeldinger`);
             yield put(smSykmeldingerHentet(data));
         } catch (e) {
             log(e);
@@ -51,7 +71,7 @@ export function* bekreftSmSykmeldingLestSaga(action) {
     if (toggle && skalBekrefte) {
         yield put(bekrefterLestSmSykmelding());
         try {
-            yield call(post, `${hentSyfoApiUrl(API_NAVN.SYFOSMREGISTER)}/v1/sykmeldinger/${action.smSykmelding.id}/bekreft`);
+            yield call(post, `${hentSykmeldingsregisterUrl()}/v1/sykmeldinger/${action.smSykmelding.id}/bekreft`);
             yield put(smSykmeldingBekreftetLest(action.smSykmelding));
             browserHistory.push('/sykefravaer');
             yield delay(10000);

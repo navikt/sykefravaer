1315b896ab5f004a5ebed1240e513977ea423b33
Johan Rusvik <johan.rusvik@nav.no>
2020-01-24 13:28:54 +0100

    Hensyntar hvilket cluster man er i når url tilVeien til Arbeid utledes

--- js/arbeidsrettet-oppfolging/HarAlleredeOppfolgingAlertstripe.js
+++ js/arbeidsrettet-oppfolging/HarAlleredeOppfolgingAlertstripe.js
@@ -4,11 +4,14 @@ import { getLedetekst } from '@navikt/digisyfo-npm';
 import React from 'react';
 
 const HarAlleredeOppfolgingAlertstripe = () => {
+    const urlVeientilarbeid = process.env.NAIS_CLUSTER_NAME === 'dev-sbs'
+        ? 'https://veientilarbeid-q.nav.no'
+        : 'https://veientilarbeid.nav.no';
     return (
         <Alertstripe type="suksess" className="alleredeOppfolging blokk">
             <div className="alleredeOppfolging__innhold">
                 <Normaltekst className="alleredeOppfolging__tekst">{getLedetekst('infoside-fo.alertstripe.tekst')}</Normaltekst>
-                <a className="lenke" href="https://veientilarbeid.nav.no">
+                <a className="lenke" href={urlVeientilarbeid}>
                     {getLedetekst('infoside-fo.alertstripe.knapp-tekst')}
                 </a>
             </div>

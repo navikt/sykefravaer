893f6239702f5bca2d1bc27ba52bef83e0ba6f5d
Tor-Christian Tangenes <torchrct@gmail.com>
2020-03-25 13:33:05 +0100

    Formattering paa drop-down infoboks

--- js/aktivitetskrav/komponenter/InfoboksAktivitetskrav.js
+++ js/aktivitetskrav/komponenter/InfoboksAktivitetskrav.js
@@ -8,26 +8,42 @@ import { getVisning, getSykefravaerVarighet } from '../../landingsside/tidslinje
 
 const tekster = {
     tittel: 'Aktivitetskrav i sykefraværet',
-    innhold: 'Vanligvis vurderer NAV om du oppfyller aktivitetskravet når du har vært sykmeldt i 8 uker. Under korona-epidemien går hensynet'
-             + 'til smittevern foran kravet til aktivitet. Derfor vilvi ikke kreve at du skal være i aktivitet utenfor hjemmet ditt. Kan du utføre'
-             + 'noe arbeid hjemmefra, bør du holde kontakt med arbeidsgiveren din. Hvis du ikke har arbeidsgiver, og du ikke kan utføre aktiviteter'
-             + 'som du har avtalt med NAV hjemmefra, vil du også få unntak fra aktivitetskravet. Du trenger ikke ta kontakt med NAV nå.',
+    aktivitetskravParagraf1: 'Vanligvis vurderer NAV om du oppfyller aktivitetskravet når du har vært sykmeldt i 8 uker.',
+    aktivitetskravParagraf2: 'Under korona-epidemien går hensynet til smittevern foran kravet til aktivitet. Derfor vil '
+                           + 'vi ikke kreve at du skal være i aktivitet utenfor hjemmet ditt.',
+    aktivitetskravParagraf3: 'Kan du utføre noe arbeid hjemmefra, bør du holde kontakt med arbeidsgiveren din.',
+    aktivitetskravParagraf4: 'Hvis du ikke har arbeidsgiver, og du ikke kan utføre aktiviteter som du har avtalt med NAV '
+                           + 'hjemmefra, vil du også få unntak fra aktivitetskravet.',
+    aktivitetskravParagraf5: 'Du trenger ikke ta kontakt med NAV nå.',
 };
 
 const sykeforlopHarPassertAktivitetsvarsel = (state) => {
     const { startdato } = state.sykeforloep;
-    console.log('STARTDATO: ', startdato);
     if (!startdato) {
         return false;
     }
 
     const antallDagerIForlopet = getSykefravaerVarighet(state);
     if (getVisning(state) === MED_ARBEIDSGIVER) {
-        console.log('MED AG: ', (antallDagerIForlopet >= 43 && antallDagerIForlopet <= 56));
         return (antallDagerIForlopet >= 43 && antallDagerIForlopet <= 56);
     }
-    console.log('UTEN AG: ', (antallDagerIForlopet >= 17 && antallDagerIForlopet <= 56));
     return (antallDagerIForlopet >= 17 && antallDagerIForlopet <= 56);
+};
+
+const InfoboksTekst = () => {
+    return (
+        <React.Fragment>
+            <p>
+                {tekster.aktivitetskravParagraf1}
+                <br />
+                {tekster.aktivitetskravParagraf2}
+                <br />
+                {tekster.aktivitetskravParagraf3}
+            </p>
+            <p>{tekster.aktivitetskravParagraf4}</p>
+            <p>{tekster.aktivitetskravParagraf5}</p>
+        </React.Fragment>
+    );
 };
 
 const InfoboksAktivitetskravComponent = (props) => {
@@ -40,7 +56,7 @@ const InfoboksAktivitetskravComponent = (props) => {
                     className="blokk"
                     variant="lyserod"
                     Overskrift="h2">
-                    { tekster.innhold }
+                    <InfoboksTekst />
                 </Utvidbar>
             ) }
         </div>

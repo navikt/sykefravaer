88477488cf82bd388d398a5bf5b4a09db85b3853
Jonas Henie <jonas.henie@nav.no>
2020-03-29 10:56:00 +0200

    Correct egenmelding register url

--- js/sider/KoronaComponents/koronaUtils.js
+++ js/sider/KoronaComponents/koronaUtils.js
@@ -7,12 +7,12 @@ export const hentSendingURL = () => {
 
     if (url.indexOf('tjenester.nav') > -1) {
         // Prod
-        return 'https://egenmeldt-sm-backendproxy.nav.no';
+        return 'https://egenmeldt-sm-backendproxy.nav.no/api/v1';
     }
     if (url.indexOf('localhost') > -1 || url.indexOf('herokuapp') > -1) {
         // Lokalt
         return '/egenmeldt-sykmelding-backend';
     }
     // Preprod
-    return 'https://egenmeldt-sm-backendproxy-q.nav.no';
+    return 'https://egenmeldt-sm-backendproxy-q.nav.no/api/v1';
 };

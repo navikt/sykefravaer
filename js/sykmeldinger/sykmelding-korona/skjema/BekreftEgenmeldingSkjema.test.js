f0a7d9f67ee483fd0b2244efc5a31b8d07834423
Sebastian Knudsen <sebastian.knudsen@nav.no>
2020-04-01 09:12:10 +0200

    viser feilmelding ved nei

--- js/sykmeldinger/sykmelding-korona/skjema/BekreftEgenmeldingSkjema.test.js
+++ js/sykmeldinger/sykmelding-korona/skjema/BekreftEgenmeldingSkjema.test.js
@@ -1,7 +1,7 @@
 import chai from 'chai';
 import sinon from 'sinon';
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import chaiEnzyme from 'chai-enzyme';
 import { RadioPanelGruppe } from 'nav-frontend-skjema';
 import { BekreftEgenmeldingSkjemaComponent } from './BekreftEgenmeldingSkjema';
@@ -23,9 +23,31 @@ const props = {
 };
 
 describe('BekreftEgenmeldingSkjema', () => {
-    it('Vise avbrytfeilmelding når man trykker på nei', () => {
-        const component = shallow(<BekreftEgenmeldingSkjemaComponent props={props} />);
+    it('Viser feilmelding når man trykker på nei', () => {
+        const component = mount(
+            <BekreftEgenmeldingSkjemaComponent props={props} />,
+        );
         expect(component.find(RadioPanelGruppe)).to.have.length(2);
         // expect(component.find(RadioPanelGruppe).first().text()).to.contain('Er opplysningene riktige?');
+        expect(
+            component
+                .find('input')
+                .at(0)
+                .prop('value'),
+        ).to.equal('ja');
+        expect(
+            component
+                .find('input')
+                .at(1)
+                .prop('value'),
+        ).to.equal('nei');
+        expect(component.find('.skjemaelement__feilmelding').length).to.equal(0);
+        component
+            .find('input')
+            .at(0)
+            .simulate('change', {
+                target: { name: 'erOpplysningeneRiktige', value: 'nei' },
+            });
+        expect(component.find('.skjemaelement__feilmelding').length).to.equal(1);
     });
 });

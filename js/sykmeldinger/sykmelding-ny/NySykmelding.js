a3f6f4c57e6501546658f2867fee2d4509fbdb20
Sebastian <sebastian.knudsen@nav.no>
2021-01-27 16:05:10 +0100

    Merknad (#406)
    * show merknad to user
    
    * fix lint errorsg
    
    * fix typo
    
    * fix typo
    
    * update digisyfo-npm
    
    * update digisyfo-npm
    
    * display title 'tilbakedatert sykmelding' if sykmelding has merknad. add 'ga til utfyllingen' button
    
    * remove test code

--- js/sykmeldinger/sykmelding-ny/NySykmelding.js
+++ js/sykmeldinger/sykmelding-ny/NySykmelding.js
@@ -2,6 +2,7 @@ import React, { Component, createRef } from 'react';
 import {
     Bjorn, DineSykmeldingOpplysninger, getLedetekst, scrollTo,
 } from '@navikt/digisyfo-npm';
+import Veilederpanel from 'nav-frontend-veilederpanel';
 import DinSykmeldingSkjemaContainer from './sykmelding-skjema/skjema/DinSykmeldingSkjemaContainer';
 import Sidetopp from '../../components/Sidetopp';
 import { getSykmeldtFornavn } from '../../utils/sykmeldingUtils';
@@ -9,7 +10,11 @@ import SykmeldingContext from '../contexts/SykmeldingContext';
 import EldreSykmeldingVarsel from '../eldre-sykmelding-varsel/EldreSykmeldingVarsel';
 import { NySykmeldingTrigger } from '../../components/HotjarTrigger';
 import PapirsykmeldingPanel from './PapirsykmeldingPanel';
+import MannNoytral from '../../components/svg/MannNoytral';
 
+const harMerknad = (sykmelding, merknadType) => { return sykmelding.merknader && sykmelding.merknader.some((merknad) => { return merknad.type === merknadType; }); };
+
+/* eslint-disable max-len */
 class NySykmelding extends Component {
     constructor() {
         super();
@@ -23,11 +28,56 @@ class NySykmelding extends Component {
                     ({ sykmelding }) => {
                         return (
                             <NySykmeldingTrigger>
-                                <Sidetopp tittel={getLedetekst('din-sykmelding.tittel')} />
+                                <Sidetopp tittel={harMerknad(sykmelding, 'UGYLDIG_TILBAKEDATERING') ? 'Tilbakedatert sykmelding' : 'Sykmelding'} />
                                 <EldreSykmeldingVarsel sykmelding={sykmelding} />
+
+                                { harMerknad(sykmelding, 'UGYLDIG_TILBAKEDATERING')
+                                    && (
+                                        <div style={{ paddingTop: '1rem', marginBottom: '2rem' }}>
+                                            <Veilederpanel
+                                                fargetema="advarsel"
+                                                type="plakat"
+                                                center
+                                                kompakt
+                                                svg={<MannNoytral />}
+                                            >
+                                                <h2>
+                                                Tilbakedateringen kan ikke godkjennes
+                                                </h2>
+                                                <p>
+                                                Vanligvis starter sykmeldingen den datoen du er hos behandleren. I enkelte tilfeller kan datoen i sykmeldingen settes tilbake i tid, det vi kaller tilbakedatering. NAV vurderer om det er en gyldig grunn for tilbakedateringen.
+                                                </p>
+                                                <p>
+                                                Sykmeldingen din er datert til før du oppsøkte behandleren, og det er ikke oppgitt noen gyldig grunn. Derfor vil du ikke få sykepenger for disse dagene.
+                                                </p>
+                                                <h2>
+                                                Hva gjør jeg nå?
+                                                </h2>
+                                                <p>
+                                                Du kan likevel sende inn sykmeldingen. Når perioden er over, sender du søknaden om sykepenger. Når søknaden er behandlet, vil du få en begrunnelse for hvorfor du ikke kan få sykepenger for de dagene, og du får samtidig mulighet til å klage.
+                                                </p>
+                                                <button
+                                                    type="button"
+                                                    onClick={(e) => {
+                                                        e.preventDefault();
+                                                        scrollTo(this.skjemaRef.current);
+                                                        this.skjemaRef.current.focus();
+                                                    }}
+                                                    className="knapp knapp--mini"
+                                                >
+                                                    Gå til utfyllingen
+                                                </button>
+                                            </Veilederpanel>
+                                        </div>
+                                    )
+                                }
+
                                 { sykmelding.erPapirsykmelding
-                                    ? <PapirsykmeldingPanel sykmelding={sykmelding} skjemaRef={this.skjemaRef} />
-                                    : (
+                                    && <PapirsykmeldingPanel sykmelding={sykmelding} skjemaRef={this.skjemaRef} />
+                                }
+
+                                { !sykmelding.erPapirsykmelding && !harMerknad(sykmelding, 'UGYLDIG_TILBAKEDATERING')
+                                    && (
                                         <Bjorn
                                             className="blokk"
                                             hvit
@@ -50,13 +100,14 @@ class NySykmelding extends Component {
                                                         }}
                                                         className="knapp knapp--mini"
                                                     >
-                                                Gå til utfyllingen
+                                            Gå til utfyllingen
                                                     </button>
                                                 </p>
                                             </div>
                                         </Bjorn>
                                     )
                                 }
+
                                 <article>
                                     <header className="panelHeader panelHeader--lysebla">
                                         <img className="panelHeader__ikon" src={`${process.env.REACT_APP_CONTEXT_ROOT}/img/svg/person.svg`} alt="Du" />

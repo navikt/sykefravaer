3e5e8cf5369809318c9f771cdce87ac42258a862
Sebastian Knudsen <sebastian.knudsen@nav.no>
2021-02-05 10:25:08 +0100

    Display information when sykmelding has merknad

---
+++ js/sykmeldinger/sykmelding-ny/MerknadBanner.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import Veilederpanel from 'nav-frontend-veilederpanel';
+import { scrollTo } from '@navikt/digisyfo-npm';
+import PropTypes from 'prop-types';
+import MannNoytral from '../../components/svg/MannNoytral';
+import { sykmelding as sykmeldingPt } from '../../propTypes';
+
+export const harMerknad = (sykmelding, merknadType) => {
+    return (
+        sykmelding.merknader
+    && sykmelding.merknader.some((merknad) => {
+        return merknad.type === merknadType;
+    })
+    );
+};
+
+const VeilederpanelWrapper = ({ children, skjemaRef }) => {
+    return (
+        <div style={{ paddingTop: '1rem', marginBottom: '2rem' }}>
+            <Veilederpanel
+                fargetema="advarsel"
+                type="plakat"
+                center
+                kompakt
+                svg={<MannNoytral />}
+            >
+                {children}
+                <button
+                    type="button"
+                    onClick={(e) => {
+                        e.preventDefault();
+                        scrollTo(skjemaRef.current);
+                        skjemaRef.current.focus();
+                    }}
+                    className="knapp knapp--mini"
+                >
+          Gå til utfyllingen
+                </button>
+            </Veilederpanel>
+        </div>
+    );
+};
+
+VeilederpanelWrapper.propTypes = {
+    children: PropTypes.element,
+    skjemaRef: PropTypes.oneOfType([
+        PropTypes.func,
+        PropTypes.shape({ current: PropTypes.instanceOf(Object) }),
+    ]),
+};
+const MerknadBanner = ({ sykmelding, skjemaRef }) => {
+    if (harMerknad(sykmelding, 'UGYLDIG_TILBAKEDATERING')) {
+        return (
+            <VeilederpanelWrapper skjemaRef={skjemaRef}>
+                <h2 className="js-merknad-banner-title">
+          Tilbakedateringen kan ikke godkjennes
+                </h2>
+                <p>
+          Vanligvis starter sykmeldingen den datoen du er hos behandleren. I
+          enkelte tilfeller kan datoen i sykmeldingen settes tilbake i tid, det
+          vi kaller tilbakedatering. NAV vurderer om det er en gyldig grunn for
+          tilbakedateringen.
+                </p>
+                <p>
+          Sykmeldingen din er datert til før du oppsøkte behandleren, og det er
+          ikke oppgitt noen gyldig grunn. Derfor vil du ikke få sykepenger for
+          disse dagene.
+                </p>
+                <h2>Hva gjør jeg nå?</h2>
+                <p>
+          Du kan likevel sende inn sykmeldingen. Når perioden er over, sender du
+          søknaden om sykepenger. Når søknaden er behandlet, vil du få en
+          begrunnelse for hvorfor du ikke kan få sykepenger for de dagene, og du
+          får samtidig mulighet til å klage.
+                </p>
+            </VeilederpanelWrapper>
+        );
+    }
+
+    if (harMerknad(sykmelding, 'KREVER_FLERE_OPPLYSNINGER')) {
+        return (
+            <VeilederpanelWrapper skjemaRef={skjemaRef}>
+                <h2 className="js-merknad-banner-title">Behov for mer opplysninger</h2>
+                <p>
+          Sykmeldingen din starter tidligere enn den dagen du var hos
+          behandleren. Vi kontakter nå behandleren din for å få opplysninger om
+          hvorfor sykmeldingen er datert tilbake.
+                </p>
+                <p>
+          Du kan likevel sende inn søknaden om sykepenger. Avhengig av hvilke
+          opplysninger vi før fra behandleren din, kan det hende du ikke før
+          sykepenger for dagene før sykmeldingstidspunktet.
+                </p>
+            </VeilederpanelWrapper>
+        );
+    }
+
+    return null;
+};
+
+MerknadBanner.propTypes = {
+    sykmelding: sykmeldingPt,
+    skjemaRef: PropTypes.oneOfType([
+        PropTypes.func,
+        PropTypes.shape({ current: PropTypes.instanceOf(Object) }),
+    ]),
+};
+
+export default MerknadBanner;
---
+++ js/sykmeldinger/sykmelding-ny/MerknadBanner.test.js
@@ -0,0 +1,50 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import MerknadBanner from './MerknadBanner';
+import expect from '../../../test/expect';
+
+describe('MerknadBanner', () => {
+    it('Skal vise riktig innhold ved UGYLDIG_TILBAKEDATERING merknad', () => {
+        const sykmelding = {
+            merknader: [
+                {
+                    type: 'UGYLDIG_TILBAKEDATERING',
+                },
+            ],
+        };
+
+        const component = shallow(<MerknadBanner sykmelding={sykmelding} />);
+
+        expect(component.find('.js-merknad-banner-title')).to.contain(
+            'Tilbakedateringen kan ikke godkjennes',
+        );
+    });
+
+    it('Skal vise riktig innhold ved KREVER_FLERE_OPPLYSNINGER merknad', () => {
+        const sykmelding = {
+            merknader: [
+                {
+                    type: 'KREVER_FLERE_OPPLYSNINGER',
+                },
+            ],
+        };
+
+        const component = shallow(<MerknadBanner sykmelding={sykmelding} />);
+
+        expect(component.find('.js-merknad-banner-title')).to.contain(
+            'Behov for mer opplysninger',
+        );
+    });
+
+    it('Skal ikke vise noe manglende merknad', () => {
+        const sykmelding = {
+            merknader: [],
+        };
+
+        const component = shallow(<MerknadBanner sykmelding={sykmelding} />);
+
+        // linter complains about empty "being" a function when it's not
+        // eslint-disable-next-line no-unused-expressions
+        expect(component).to.be.empty;
+    });
+});

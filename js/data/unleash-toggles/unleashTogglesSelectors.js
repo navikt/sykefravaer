91f9d25e3d49a19e0647c87b8ca2d1ed6261f879
Tia Firing <tia.firing@nav.no>
2021-03-05 18:32:39 +0100

    Revert "Merge pull request #417 from navikt/arbeidsforhold"
    This reverts commit 050dba591a99c80ffed289bcc13d9d397fd8c703, reversing
    changes made to 2765e364d09a655c929c22c9616ddeced916b549.

--- js/data/unleash-toggles/unleashTogglesSelectors.js
+++ js/data/unleash-toggles/unleashTogglesSelectors.js
@@ -15,7 +15,6 @@ import {
     NYTT_SYKMELDINGSMOTTAK,
     NGINX_PROXY,
     SYKMELDINGER_BACKEND,
-    SYKMELDINGER_BACKEND_ARB,
 } from '../../enums/unleashToggles';
 
 export const toggleSykmeldingEndreArbeidssituasjon = (state) => {
@@ -45,9 +44,4 @@ export const toggleBrukNginxProxy = (state) => {
 export const toggleSykmeldingerBackend = (state) => {
     return !state.unleashToggles.hentingFeilet
         && state.unleashToggles.data[SYKMELDINGER_BACKEND] === true;
-};
-
-export const toggleSykmeldingerBackendArbeidsforhold = (state) => {
-    return !state.unleashToggles.hentingFeilet
-        && state.unleashToggles.data[SYKMELDINGER_BACKEND_ARB] === true;
 };
--- js/sykmeldinger/data/arbeidsgivere/arbeidsgivereSagas.js
+++ js/sykmeldinger/data/arbeidsgivere/arbeidsgivereSagas.js
@@ -3,36 +3,9 @@ import {
 } from 'redux-saga/effects';
 import { get, log } from '../../../digisyfoNpm';
 import * as actions from './arbeidsgivereActions';
-import { toggleSykmeldingerBackendArbeidsforhold, unleashtogglesHentetSelector } from '../../../data/unleash-toggles/unleashTogglesSelectors';
-import { HENTET_UNLEASH_TOGGLES } from '../../../data/unleash-toggles/unleashToggles_actions';
-import { erNaisLabsDemo, getSyforestRoot } from '../../../utils/urlUtils';
+import { getSyforestRoot } from '../../../utils/urlUtils';
 
 const { HENT_AKTUELLE_ARBEIDSGIVERE_FORESPURT } = actions;
-
-const getSykmeldingerBackendUrl = () => {
-    const url = window
-    && window.location
-    && window.location.href
-        ? window.location.href
-        : '';
-
-    if (url.indexOf('tjenester.nav') > -1) {
-        // Prod
-        return 'https://sykmeldinger-backend-proxy.nav.no/api/v1/syforest/arbeidsforhold';
-    }
-    if (url.indexOf('localhost:2027') > -1) {
-        return 'http://localhost:6998/api/v1/syforest/arbeidsforhold';
-    }
-    if (url.indexOf('localhost:2028') > -1) {
-        return 'http://localhost:6998/api/v1/syforest/arbeidsforhold';
-    }
-    if (url.indexOf('localhost') > -1 || erNaisLabsDemo()) {
-        // Lokalt
-        return '/sykmeldinger-backend';
-    }
-    // Preprod
-    return 'https://sykmeldinger-backend-proxy.dev.nav.no/api/v1/syforest/arbeidsforhold';
-};
 
 export const skalHenteArbeidsgivere = (state, sykmeldingId) => {
     return state.arbeidsgivere.sykmeldingId !== sykmeldingId;
@@ -42,24 +15,19 @@ export function* hentDineArbeidsgivere(action) {
     const { sykmeldingId } = action;
     const skalHente = yield select(skalHenteArbeidsgivere, sykmeldingId);
     if (skalHente) {
-        const togglesHentet = yield select(unleashtogglesHentetSelector);
-        const toggleNewUrl = yield select(toggleSykmeldingerBackendArbeidsforhold);
-        if (togglesHentet) {
-            const url = toggleNewUrl ? `${getSykmeldingerBackendUrl()}` : `${getSyforestRoot()}/informasjon/arbeidsgivere`;
-            yield put(actions.henterAktuelleArbeidsgivere(sykmeldingId));
-            try {
-                const data = yield call(get, `${url}?sykmeldingId=${sykmeldingId}`);
-                yield put(actions.aktuelleArbeidsgivereHentet(sykmeldingId, data));
-            } catch (e) {
-                log(e);
-                yield put(actions.hentAktuelleArbeidsgivereFeilet(sykmeldingId));
-            }
+        yield put(actions.henterAktuelleArbeidsgivere(sykmeldingId));
+        try {
+            const data = yield call(get, `${getSyforestRoot()}/informasjon/arbeidsgivere?sykmeldingId=${sykmeldingId}`);
+            yield put(actions.aktuelleArbeidsgivereHentet(sykmeldingId, data));
+        } catch (e) {
+            log(e);
+            yield put(actions.hentAktuelleArbeidsgivereFeilet(sykmeldingId));
         }
     }
 }
 
 function* watchHentArbeidsgivere() {
-    yield takeEvery([HENT_AKTUELLE_ARBEIDSGIVERE_FORESPURT, HENTET_UNLEASH_TOGGLES], hentDineArbeidsgivere);
+    yield takeEvery(HENT_AKTUELLE_ARBEIDSGIVERE_FORESPURT, hentDineArbeidsgivere);
 }
 
 export default function* arbeidsgivereSagas() {

0769471e833a64e459152ead9ec3d2f80849d7aa
Sebastian Knudsen <sebastian.knudsen@nav.no>
2021-04-13 16:34:51 +0200

    Synch mock data between sykmeldinger and arbeidsgiversSykmeldinger

--- mock/mockSykmeldingerBackend.js
+++ mock/mockSykmeldingerBackend.js
@@ -1,20 +1,24 @@
-const mockData = require('./mockData');
-const enums = require('./mockDataEnums');
+const mockData = require("./mockData");
+const enums = require("./mockDataEnums");
 
 // eslint-disable-next-line no-unused-vars
 const mockSykmeldingerBackend = (server, erLokal) => {
-    server.get('/sykmeldinger-backend/sykmeldinger', (req, res) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify(mockData[enums.SYKMELDINGER]));
-    });
-    server.get('/sykmeldinger-backend/brukerinformasjon', (req, res) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify({ navn: 'Ola', alder: 32 }));
-    });
-    server.get('/sykmeldinger-backend/arbeidsforhold', (req, res) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify(mockData[enums.ARBEIDSGIVERE]));
-    });
+  server.get("/sykmeldinger-backend/sykmeldinger", (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    if (req.query.type === "arbeidsgiver") {
+      res.send(JSON.stringify(mockData[enums.ARBEIDSGIVERS_SYKMELDINGER]));
+    } else {
+      res.send(JSON.stringify(mockData[enums.SYKMELDINGER]));
+    }
+  });
+  server.get("/sykmeldinger-backend/brukerinformasjon", (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.send(JSON.stringify({ navn: "Ola", alder: 32 }));
+  });
+  server.get("/sykmeldinger-backend/arbeidsforhold", (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.send(JSON.stringify(mockData[enums.ARBEIDSGIVERE]));
+  });
 };
 
 module.exports = mockSykmeldingerBackend;
